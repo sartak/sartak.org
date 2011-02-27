8f90810c76a6c52c4af169ff211678346c8491b7
Shawn M Moore <sartak@bestpractical.com>
2011-02-27 17:48:53 -0500

    Move __END__ POD into prose

--- writing/plugins-are-objects-not-roles.pl
+++ writing/plugins-are-objects-not-roles.pl
@@ -36,58 +36,31 @@ h3 { "Plugins as Objects with Roles" };
 
 h3 { "Fight!" };
 
-__END__
+# separation of concerns
+p { "Treating plugins as roles, as in MXOP, encourages the \"god object\" design antipattern. Because [MooseX::Plugins] plugins are delegate objects and are not composed into the owner object, separation of concerns is neatly maintained." };
 
-=item separation of concerns
+#modularization
+p { "Each plugin, because it's a class, gets its own namespace, so plugin authors do not need to worry about depleting or polluting the owner's namespace." };
 
-Treating plugins as roles, as in MXOP, encourages the "god object" design
-antipattern. Because L<MooseX::Plugins> plugins are delegate objects and are
-not composed into the owner object, separation of concerns is neatly maintained.
+#clean initialization
 
-=item modularization
+p { "In MXOP, plugins are added to an already initialized object, so `required`, `default`, `BUILD`, etc., do not work out of the box. Instead, you must remember to use `lazy` for your plugins' attributes, and possibly even extend the role metaobject code to add the concept of post-apply hooks to roles. But when plugins are classes, each undergoes the complete object initialization process." };
 
-Each plugin, because it's a class, gets its own namespace, so plugin authors do
-not need to worry about depleting or polluting the owner's namespace.
+# removal
 
-=item clean initialization
+p { "As of this writing there is no way to remove a role from a class. I don't see anything that makes this technically impossible, but in addition to being a lot more possible, it's also a lot simpler to just remove a plugin from a list of objects." };
 
-In MXOP, plugins are added to an already initialized object, so C<required>,
-C<default>, C<BUILD>, etc., do not work out of the box. Instead, you must
-remember to use C<lazy> for your plugins' attributes, and possibly even extend
-the role metaobject code to add the concept of post-apply hooks to roles.
+#role theory
 
-But when plugins are classes, each undergoes the complete object initialization
-process.
+p { "`MooseX::Object::Pluggable` relies heavily on method modifiers for actually augmenting behavior. Method modifiers are an unclean abuse of role theory and are implemented only in Moose for convenience - modifiers violate some of the most important principles of role theory (which is that role application order should never matter)." };
 
-=item removal
+# TODO add a note that MXP is a model citizen of role land
 
-As of this writing there is no way to remove a role from a class. I don't see
-anything that makes this technically impossible, but in addition to being a lot
-more possible, it's also a lot simpler to just remove a plugin from a list of
-objects.
+#introspection
 
-=item role theory
+p { "Plugins must consume roles to effect any behavior changes. Because consuming a role declares particular semantics. Simply wrapping methods in a god object does not declare any semantics." };
 
-L<MooseX::Object::Pluggable> relies heavily on method modifiers for actually
-augmenting behavior. Method modifiers are an unclean abuse of role theory and
-are implemented only in Moose for convenience - modifiers violate some of the
-most important principles of role theory (which is that role application order
-should never matter).
+# refactorability
 
-(todo: add a note that MXP is a model citizen of role land)
+p { "`MooseX::Object::Pluggable` relies primarily on method modifiers to achieve behavior extension. This means that the application designer cannot simply refactor methods that are wrapped by plugins without breaking them. The application designer has complete control over L<MooseX::Plugins> hook points. Moving a hook point in use by several plugins can be done safely and without backwards compatibility issues." };
 
-=item introspection
-
-Plugins must consume roles to effect any behavior changes. Because consuming a
-role declares particular semantics. Simply wrapping methods in a god object
-does not declare any semantics.
-
-=item refactorability
-
-L<MooseX::Object::Pluggable> relies primarily on method modifiers to achieve
-behavior extension. This means that the application designer cannot simply
-refactor methods that are wrapped by plugins without breaking them.
-
-The application designer has complete control over L<MooseX::Plugins> hook
-points. Moving a hook point in use by several plugins can be done safely and
-without backwards compatibility issues.
