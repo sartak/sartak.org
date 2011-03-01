199adb8ca77fac6e7dbb174b307018a545862ff6
Shawn M Moore <sartak@bestpractical.com>
2011-03-01 04:41:21 -0500

    Explain the eolws killer

--- writing/eol-whitespace-in-vim.pl
+++ writing/eol-whitespace-in-vim.pl
@@ -11,7 +11,7 @@ set list
 set listchars=trail:.
 EOV
 
-p { "These two settings make it so when whitespace occurs at the end of a line, each character is displayed as a blue period. This is good in theory but in practice it means that whenever you're typing new content, these blue periods show up whenever you're done typing a word and continue on to the next one. This is what I mean by _obnoxious_. Of course there's going to be lots of fleeting EOL whitespace if I'm typing a sentence or a line of code. I don't need to be distracted by a blue dot bouncing around with my cursor. So a few years ago [Jesse Luehrs](http://tozt.net) and I banged our heads against our own separate walls until we found something that worked for us:" };
+p { "These two settings make it so when whitespace occurs at the end of a line, each character is displayed as a blue period. This is good in theory but in practice it means that whenever you're typing new content, these blue periods show up whenever you're done typing a word and continue on to the next one. This is what I mean by _obnoxious_. Of course there's going to be lots of fleeting EOL whitespace if I'm typing a sentence or a line of code. I don't need to be distracted by a blue dot bouncing around with my cursor. So a few years ago [Jesse Luehrs](http://tozt.net) and I banged our heads against our own separate walls until we came up with something that worked for us:" };
 
 code_snippet vim => << 'EOV';
 autocmd InsertEnter * syn clear EOLWS | syn match EOLWS excludenl /\s\+\%#\@!$/
@@ -31,5 +31,7 @@ code_snippet vim => << 'EOV';
 nmap <leader>w :%s/\s\+$//<CR>:let @/=''<CR>
 EOV
 
-p { "The `:let @/=''` bit empties the `/` register so that the whitespace you deleted is not used accidentally in an :hlsearch." };
+p { "`nmap` sets up a normal mode command remapping (see also `imap`, `vmap`, etc). The `<leader>` is the user-configurable modifier key to provide you with a sort of namespace for your own commands. By default the leader is comma but I like backslash better. So `<leader>w` remaps the `\\w` in normal mode to run everything after the new command key sequence. When you type `\\w` then vim will act as if you had typed `:%s/\\s\\+\$//<CR>:let \@/=''<CR>`." };
+
+p { "The first part should be familiar to anyone who's used vim seriously. `:%s/\\s\\+\$//` just runs a substitution to remove all whitespace characters that occur at end of line. `<CR>` is a way to mock typing the Enter key in vimscript. The `:let @/=''` bit empties the `/` register so that the whitespace you deleted is not used accidentally in an :hlsearch." };
 
