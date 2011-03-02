147b0ede71a07e620c6190e76d274ffe931b2ef9
Shawn M Moore <sartak@bestpractical.com>
2011-03-01 21:15:30 -0500

    Last tweaks

--- writing/eol-whitespace-in-vim.pl
+++ writing/eol-whitespace-in-vim.pl
@@ -2,7 +2,7 @@ use Sartak::Blog;
 
 BEGIN { print "title: End-of-Line Whitespace in Vim\ndraft: 1\n" }
 
-p { "Whitespace characters at the ends of lines are sloppy and useless. Not to mention when you notice and remove them, they clutter up your version control history, though this can be mitigated somewhat by using `git diff --ignore-space-at-eol` or similar. So I have two settings in [my vimrc](https://github.com/sartak/conf/blob/master/vimrc) that help me avoid committing EOL whitespace to any of my hobby or work projects." };
+p { "Whitespace characters at the ends of lines are sloppy and almost 100% useless. Not to mention when you notice and remove them, they clutter up your version control history. Though that can be mitigated somewhat by using `git diff --ignore-space-at-eol` or similar, it's better to never let it become a problem. So I have two settings in [my vimrc](https://github.com/sartak/conf/blob/master/vimrc) that help me avoid committing EOL whitespace to any of my hobby or work projects." };
 
 p { "The first one highlights EOL whitespace so you can tell that it's even there, but in a way that isn't _obnoxious_. Vim does offer a builtin option to do this. But it sucks." };
 
@@ -11,7 +11,7 @@ set list
 set listchars=trail:.
 EOV
 
-p { "These two settings make it so when whitespace occurs at the end of a line, each character is displayed as a blue period. This is good in theory, but actually it means that whenever you're typing new content, these blue periods show up every single time you're done typing a word and continue on to the next one. This is what I mean by _obnoxious_. Of course there's going to be lots of fleeting EOL whitespace if I'm typing a sentence or a line of code. So a few years ago [Jesse Luehrs](http://tozt.net) and I banged our heads against our own separate walls until we came up with something that worked for us:" };
+p { "These two settings make it so when whitespace occurs at the end of a line, each character is displayed as a blue period. This is good in theory, but actually it means that whenever you're typing new content, these blue periods show up every single time you're done typing a word and continue on to the next one. This is what I mean by _obnoxious_. Of course there's going to be lots of fleeting EOL whitespace if I'm typing a sentence or a line of code! So a few years ago [Jesse Luehrs](http://tozt.net) and I banged our heads against our own separate walls until we came up with something that worked for us. I haven't seen this used anywhere else but it works perfectly for us so I wanted to share it." };
 
 code_snippet vim => << 'EOV';
 autocmd InsertEnter * syn clear EOLWS | syn match EOLWS excludenl /\s\+\%#\@!$/
@@ -21,7 +21,7 @@ EOV
 
 p { "What this does is highlight EOL whitespace with a red background **except on the line you're editing**. Which means it's not obnoxiously telling you about every time you hit the space bar. The highlighting only occurs when you leave insert mode or if you move the cursor to a different line, such as by hitting enter." };
 
-p { "The `syn clear EOLS` is needed so when you switch modes the appropriate syntax highlighting rule runs. The first line has two components of abominable Vim-specific regex: `\\%#` matches cursor position, and `\@!` is like a negative lookahead from Perl for the previous atom. So in effect this matches end of line whitespace, except when the cursor is inside that whitespace." };
+p { "The `syn clear EOLS` is needed so when you switch modes, the appropriate syntax highlighting rule runs instead of both of them. The first line has two components of abominable Vim-specific regex: `\\%#` matches cursor position, and `\@!` is like a negative lookahead from Perl for the previous atom. So in effect this matches end of line whitespace, except when the cursor is inside that whitespace." };
 
 p { "The other tool I use to combat EOL whitespace is to unceremoniously execute it." };
 
