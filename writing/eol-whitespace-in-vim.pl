b431e96ae666d4e36dc101b99aef88d4a9f4888e
Shawn M Moore <sartak@bestpractical.com>
2011-03-01 21:06:19 -0500

    Basically finish the article

--- writing/eol-whitespace-in-vim.pl
+++ writing/eol-whitespace-in-vim.pl
@@ -4,7 +4,7 @@ BEGIN { print "title: End-of-Line Whitespace in Vim\ndraft: 1\n" }
 
 p { "Whitespace characters at the ends of lines are sloppy and useless. Not to mention when you notice and remove them, they clutter up your version control history, though this can be mitigated somewhat by using `git diff --ignore-space-at-eol` or similar. So I have two settings in [my vimrc](https://github.com/sartak/conf/blob/master/vimrc) that help me avoid committing EOL whitespace to any of my hobby or work projects." };
 
-p { "The first one highlights EOL whitespace so you can tell that it's even there, but in a way that isn't _obnoxious_. vim does offer a builtin option to do this. But it sucks." };
+p { "The first one highlights EOL whitespace so you can tell that it's even there, but in a way that isn't _obnoxious_. Vim does offer a builtin option to do this. But it sucks." };
 
 code_snippet vim => << 'EOV';
 set list
@@ -19,11 +19,9 @@ autocmd InsertLeave * syn clear EOLWS | syn match EOLWS excludenl /\s\+$/
 highlight EOLWS ctermbg=red guibg=red
 EOV
 
-p { "What this does is highlight EOL whitespace with a red background (we'd use a red foreground but it's whitespace, you see!) **except on the line you're editing**. Which means it's not _obnoxious_. The highlighting only occurs when you leave insert mode or if you leave that line, such as by hitting insert." };
+p { "What this does is highlight EOL whitespace with a red background **except on the line you're editing**. Which means it's not _obnoxious_. The highlighting only occurs when you leave insert mode or if you leave that line, such as by hitting insert." };
 
-p { "It's worth explaining how this bit of vimscript works. `autocmd` is used to register triggers in vim, to run some code after some _event_. In this case we want to run code on the `InsertEnter` and `InsertLeave` events. The next argument to each `autocmd` is the filename pattern. We specify `*` which indicates that we want this trigger to fire for all files, not just `*.html` or `Makefile` or whatever. Then the interesting stuff happens." };
-
-p { "`syn clear EOLWS` gets rid of the " };
+p { "The `syn clear EOLS` is needed so when you leave insert mode and go back into it, the regex with `\\%#\\@!` runs. This abomination of regex has two components: `\\%#` matches cursor position, and `\@!` is like a negative lookahead from Perl for the previous atom. So in effect this matches end of line whitespace except if the cursor is after that space." };
 
 p { "The other tool I use to combat EOL whitespace is to unceremoniously execute it. This remapping makes `\\w` (or more likely `,w` -- my leader is nonstandard) just kill all EOL whitespace." };
 
@@ -31,9 +29,7 @@ code_snippet vim => << 'EOV';
 nmap <leader>w :%s/\s\+$//<CR>:let @/=''<CR>
 EOV
 
-p { "`nmap` sets up a normal mode command remapping (see also `imap`, `vmap`, etc). The `<leader>` is the user-configurable modifier key to provide you with a sort of namespace for your own commands. By default the leader is comma but I like backslash better. So `<leader>w` remaps the `\\w` in normal mode to run everything after the new command key sequence. When you type `\\w` then vim will act as if you had typed `:%s/\\s\\+\$//<CR>:let \@/=''<CR>`." };
+p { "This sets up a new normal-mode command `<leader>w`. The `<leader>` is like a user-specific namespace for custom commands; for most people it's going to be `,` but I have it set to `\\`. The `:let @/=''` bit empties the `/` register so that regular expression is not used for the `n` command or `hlsearch` highlighting." };
 
-p { "The first part should be familiar to anyone who's used vim seriously. `:%s/\\s\\+\$//` just runs a substitution to remove all whitespace characters that occur at end of line. `<CR>` is a way to mock typing the Enter key in vimscript. The `:let @/=''` bit empties the `/` register so that the whitespace you deleted is not used accidentally in an :hlsearch." };
-
-p { "Some people run something like this in a `BufWritePre` autocommand. But I don't like that solutions because sometimes whitespace at the end of a line _is_ important -- such as in [Markdown](http://daringfireball.net/projects/markdown/syntax#p). Instead, the configuration I've described gives you tools for dealing with EOL whitespace sanely." };
+p { "Some people run a whitespace stripper like this in a `BufWritePre` autocommand. But I don't like that solution because sometimes whitespace at the end of a line _is_ important -- such as in [Markdown](http://daringfireball.net/projects/markdown/syntax#p). Instead, the configuration I've described gives you tools for dealing with EOL whitespace sanely." };
 
