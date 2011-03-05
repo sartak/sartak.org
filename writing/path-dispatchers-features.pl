b0469b5147d890f867b5b340f6d575ce6f4d6466
Shawn M Moore <sartak@bestpractical.com>
2011-03-04 20:16:53 -0500

    Link to plack-dispatching-samples

--- writing/path-dispatchers-features.pl
+++ writing/path-dispatchers-features.pl
@@ -2,7 +2,7 @@ use Sartak::Blog;
 
 BEGIN { print "title: Path::Dispatcher's Features\ndraft: 1\n" }
 
-p { "[vti's \"Perl routes dispatching comparison\"](http://showmetheco.de/articles/2011/3/perl-routes-dispatching-comparison.html) originally did not include the routing module that I wrote called `[Path::Dispatcher]`. I'm going to chalk that up to `Path::Dispatcher` not being well-known enough, hence this post. I'd like to describe some features beyond those included in vti's comparison, because `Path::Dispatcher`'s design (which shines through in its API) enables some features not supported by any of the other routing modules I've seen." };
+p { "[vti's \"Perl routes dispatching comparison\"](http://showmetheco.de/articles/2011/3/perl-routes-dispatching-comparison.html) (which is reminiscent of (miyagawa's plack-dispatching-samples)[https://github.com/miyagawa/plack-dispatching-samples]) originally did not include the routing module that I wrote called `[Path::Dispatcher]`. I'm going to chalk that up to `Path::Dispatcher` not being well-known enough, hence this post. I'd like to describe some features beyond those included in vti's comparison, because `Path::Dispatcher`'s design (which shines through in its API) enables some features not supported by any of the other routing modules I've seen." };
 
 ul {
     li { "more generic than URLs" }
@@ -15,3 +15,4 @@ ul {
     li { "including other dispatchers (sorta like subroutes)" }
 }
 
+# https://github.com/bestpractical/path-dispatcher/blob/master/t/027-custom-named-captures.t
