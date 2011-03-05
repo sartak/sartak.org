b4da9eb3fe5eca1d79a75d4af916f32d94ba4448
Shawn M Moore <sartak@bestpractical.com>
2011-03-04 19:59:48 -0500

    What I wanna talk about

--- writing/path-dispatchers-features.pl
+++ writing/path-dispatchers-features.pl
@@ -2,5 +2,16 @@ use Sartak::Blog;
 
 BEGIN { print "title: Path::Dispatcher's Features\ndraft: 1\n" }
 
-p { "[vti's \"Perl routes dispatching comparison\"](http://showmetheco.de/articles/2011/3/perl-routes-dispatching-comparison.html) originally did not include the routing module that I wrote called `[Path::Dispatcher]`. I'm going to chalk that up to `Path::Dispatcher` not being famous enough, hence this post. I'd like to describe some features beyond those included in vti's comparison, because `Path::Dispatcher`'s design enables some features not supported by any of those other routers." };
+p { "[vti's \"Perl routes dispatching comparison\"](http://showmetheco.de/articles/2011/3/perl-routes-dispatching-comparison.html) originally did not include the routing module that I wrote called `[Path::Dispatcher]`. I'm going to chalk that up to `Path::Dispatcher` not being well-known enough, hence this post. I'd like to describe some features beyond those included in vti's comparison, because `Path::Dispatcher`'s design (which shines through in its API) enables some features not supported by any of the other routing modules I've seen." };
+
+ul {
+    li { "more generic than URLs" }
+    li { "autocompletion" }
+    li { "non-regex-based constraints (Moose type constraints, coderefs)" }
+    li { "dispatch inspection (which rule matched)" }
+    li { "named captures" }
+    li { "API instead of sugar layer" }
+    li { "custom rules" }
+    li { "including other dispatchers (sorta like subroutes)" }
+}
 
