d1449ff687382c0498c9d202a4920f5f4b386f4a
Shawn M Moore <sartak@bestpractical.com>
2011-03-04 19:36:54 -0500

    Start writing Path::Dispatcher's Features

---
+++ writing/path-dispatchers-features.pl
@@ -0,0 +1,6 @@
+use Sartak::Blog;
+
+BEGIN { print "title: Path::Dispatcher's Features\ndraft: 1\n" }
+
+p { "[vti's \"Perl routes dispatching comparison\"](http://showmetheco.de/articles/2011/3/perl-routes-dispatching-comparison.html) originally did not include the routing module that I wrote called [Path::Dispatcher]. I'm going to chalk that up to `Path::Dispatcher` not being famous enough, hence this post. I'd like to describe some features beyond those included in vti's comparison, because `Path::Dispatcher`'s design enables some features not supported by any of those other routers." };
+
