47a81ab0fd2c7ddb435283a501f7149acb38782d
Shawn M Moore <sartak@bestpractical.com>
2011-04-26 11:20:04 -0400

    More words, mostly Japanese ones

--- writing/勝負という言葉を習らうにはどうしたのか.pl
+++ writing/勝負という言葉を習らうにはどうしたのか.pl
@@ -4,8 +4,11 @@ BEGIN { print "title: 勝負という言葉を習らうにはどうしたのか\
 
 p { "もっと日本語で書こうとしよう！" };
 
+p { "今朝は、Twitterで「Pokemon doesn't use kanji, but it's neat when I type in words I've learned from it and their kanji make a lot of sense. しょうぶ->勝負」と書いた。二言語で話された人しかそれを読まないので、ブログで日本語でこれを書いた。" };
+
 p { "「しょうぶしようぜ！」" };
 
 p { "ポケモンやる時に、「しょうぶ」という言葉を習った。" };
 
-# 北斗の拳
+p { "北斗の拳、ふりがながなかった" };
+
