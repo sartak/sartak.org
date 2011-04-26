6ca3149b190176201ff849b652cc772b60fd6f98
Shawn M Moore <sartak@bestpractical.com>
2011-04-26 11:35:56 -0400

    Link to my tweet

--- writing/勝負という言葉を習らうにはどうしたのか.pl
+++ writing/勝負という言葉を習らうにはどうしたのか.pl
@@ -4,7 +4,7 @@ BEGIN { print "title: 勝負という言葉を習らうにはどうしたのか\
 
 p { "もっと日本語で書こうとしよう！" };
 
-p { "今朝は、Twitterで「Pokemon doesn't use kanji, but it's neat when I type in words I've learned from it and their kanji make a lot of sense. しょうぶ->勝負」と書いた。二言語で話された人しかそれを読まないので、ブログで日本語でこれを書いた。" };
+p { "今朝は、[Twitter](http://twitter.com/#!/sartak/status/62870779360067584)で「Pokemon doesn't use kanji, but it's neat when I type in words I've learned from it and their kanji make a lot of sense. しょうぶ->勝負」と書いた。二言語で話された人しかそれを読まないので、ブログで日本語でこれを書いた。" };
 
 p { "九ヶ月前に、ポケモンやる時に「しょうぶしようぜ！」を読んで、フラッシュカードを作った。ポケモンは漢字が使わない" };
 
