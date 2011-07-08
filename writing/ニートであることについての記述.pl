c12fc08667ff3d11e2966fc416bc56201d23c1d8
Shawn M Moore <sartak@bestpractical.com>
2011-07-08 11:23:09 -0400

    Oops, Bread::Board

--- writing/ニートであることについての記述.pl
+++ writing/ニートであることについての記述.pl
@@ -4,7 +4,7 @@ BEGIN { print "title: ニートであることについての記述\ndraft: 1\n"
 
 p { "お久しぶりですね！！今日の多読を休止しようと思ったが、日常の出来事についてツイートより長い説明を書く。" };
 
-p { "第一に、先月に会社を辞めた。Infinity Interactiveに雇われた！[Moose](http://moose.perl.org)とか[KiokuDB](http://www.iinteractive.com/kiokudb/)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)をリリースした会社なんだ。ワクワクしながら始まるのを待ってる！まだ始まらなかった理由は、まる1ヶ月の休みを取りたかったんだ。7月中に激しく日本語をレベルアップしたいんだぜ！" };
+p { "第一に、先月に会社を辞めた。Infinity Interactiveに雇われた！[Moose](http://moose.perl.org)とか[KiokuDB](http://www.iinteractive.com/kiokudb/)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)とか[Bread::Board](http://beta.metacpan.org/module/Bread::Board)をリリースした会社なんだ。ワクワクしながら始まるのを待ってる！まだ始まらなかった理由は、まる1ヶ月の休みを取りたかったんだ。7月中に激しく日本語をレベルアップしたいんだぜ！" };
 
 p { "先週に[YAPC::NA](http://www.yapc2011.us/yn2011/)を参加して、2つの演説(?)やった。1つ目は、[cpandoc](http://perldoc.jp/docs/modules/Pod-Cpandoc-0.09/Cpandoc.pod)についてLTだった。インターネットが必要だった生の実演で、cpandocを使う方法を教えたが、楽しかった！2つ目は、[Announcements](http://beta.metacpan.org/module/Announcements)入門を20分話した。YAPCで、miyagawaさんが僕に「How does it feel to be a ニート?」って冗談に聞いたので、このブログの表題をくれた。" };
 
