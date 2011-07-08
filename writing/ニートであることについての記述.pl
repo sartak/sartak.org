d3f68c937265146097517e58a42d764494fb0eac
Shawn M Moore <sartak@bestpractical.com>
2011-07-08 11:21:29 -0400

    Lots more content

--- writing/ニートであることについての記述.pl
+++ writing/ニートであることについての記述.pl
@@ -4,12 +4,13 @@ BEGIN { print "title: ニートであることについての記述\ndraft: 1\n"
 
 p { "お久しぶりですね！！今日の多読を休止しようと思ったが、日常の出来事についてツイートより長い説明を書く。" };
 
-p { "第一に、先月に会社を辞めた。Infinity Interactiveに雇われた！[Moose](http://moose.perl.org)とか[KiokuDB](http://www.iinteractive.com/kiokudb/)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)をリリースした会社なんだ。ワクワクしながら始まるのを待ってる！まだ始まらなかった理由は、まる1ヶ月の休みを取りたかったんだ。7月中に激しく日本語をレベルアップしたいんだよ！" };
+p { "第一に、先月に会社を辞めた。Infinity Interactiveに雇われた！[Moose](http://moose.perl.org)とか[KiokuDB](http://www.iinteractive.com/kiokudb/)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)をリリースした会社なんだ。ワクワクしながら始まるのを待ってる！まだ始まらなかった理由は、まる1ヶ月の休みを取りたかったんだ。7月中に激しく日本語をレベルアップしたいんだぜ！" };
 
-# YAPC::NA
-# - miyagawa's ニート question
-# - cpandoc
-# - announcements
+p { "先週に[YAPC::NA](http://www.yapc2011.us/yn2011/)を参加して、2つの演説(?)やった。1つ目は、[cpandoc](http://perldoc.jp/docs/modules/Pod-Cpandoc-0.09/Cpandoc.pod)についてLTだった。インターネットが必要だった生の実演で、cpandocを使う方法を教えたが、楽しかった！2つ目は、[Announcements](http://beta.metacpan.org/module/Announcements)入門を20分話した。YAPCで、miyagawaさんが僕に「How does it feel to be a ニート?」って冗談に聞いたので、このブログの表題をくれた。" };
+
+p { "最近、DTraceを習ってみた。強力すぎなツールだから、使わなくてはいけない。printf式でデバグが好きなプログラマー、DTraceを調査して下さい！[Pinboard](http://pinboard.in)でDTraceを記述する記事を集めている: [すべて](http://pinboard.in/u:sartak/t:dtrace)[日本語だけ](http://pinboard.in/u:sartak/t:%E6%97%A5%E6%9C%AC%E8%AA%9E/)。PerlがDTraceのプローブがあるけど、関数の呼びと関数の返ししかトレースできない。しかし、僕がPerlにプローブ追加しようとしてる。一回目に、`\${^GLOBAL_PHASE}`というの変数の変化([perldocを参照](http://perldoc.perl.org/perlvar.html#%24%7b%5eGLOBAL_PHASE%7d))を知らせたい。それだから、p5pに[パッチ](https://rt.perl.org/rt3/Public/Bug/Display.html?id=94234)をあげた。今年のYAPC::Asiaで、僕の話はDTraceのテーマと思う。Pittsburgh Perl Workshopでも、もし行ったら。" };
+
+p { "" };
 
 # Study
 # - モダンPerl入門 - vocabulary
@@ -17,8 +18,4 @@ p { "第一に、先月に会社を辞めた。Infinity Interactiveに雇われ�
 # - デューン
 # - Scribblenauts, kanji game
 # - This post
-
-# DTrace
-# - new Perl probes
-# - YAPC::Asia (and possibly PPW) talk
 
