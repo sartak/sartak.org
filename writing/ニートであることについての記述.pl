6952fc0a19b953a5a08d960c302c9ac5d7dc2f58
Shawn M Moore <sartak@bestpractical.com>
2011-07-08 10:22:12 -0400

    Links, grammar fix

--- writing/ニートであることについての記述.pl
+++ writing/ニートであることについての記述.pl
@@ -4,7 +4,7 @@ BEGIN { print "title: ニートであることについての記述\ndraft: 1\n"
 
 p { "お久しぶりですね！！今日の多読を休止しようと思ったが、日常の出来事についてツイートより長い説明を書く。" };
 
-p { "第一に、先月に会社を辞めた。Infinity Interactiveに雇われた！MooseとかKiokuDBとかPath::RouterとかBread::Boardをリリースした会社なんだ。ワクワクしながら始まるのを待ってる！まだ始まらなかった理由は、まる1ヶ月の休みを取ったかったんだ。7月中に激しく日本語をレベルアップしたいんだよ！" };
+p { "第一に、先月に会社を辞めた。Infinity Interactiveに雇われた！[Moose](http://moose.perl.org)とか[KiokuDB](http://www.iinteractive.com/kiokudb/)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)とか[Path::Router](http://beta.metacpan.org/module/Path::Router)をリリースした会社なんだ。ワクワクしながら始まるのを待ってる！まだ始まらなかった理由は、まる1ヶ月の休みを取りたかったんだ。7月中に激しく日本語をレベルアップしたいんだよ！" };
 
 # YAPC::NA
 # - miyagawa's ニート question
