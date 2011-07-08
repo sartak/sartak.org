5533bd6c0674e2d2aaa270203ac0a3c3fbf85bb1
Shawn M Moore <sartak@bestpractical.com>
2011-07-08 09:24:44 -0400

    topics

--- writing/ニートであることについての記述.pl
+++ writing/ニートであることについての記述.pl
@@ -2,5 +2,26 @@ use Sartak::Blog;
 
 BEGIN { print "title: ニートであることについての記述\ndraft: 1\n" }
 
-p { "ほげ" };
+p { "" };
+
+# Quit my job
+# - New job is II
+# - Studying Japanese in July
+# - Also finding good cafes to hack both Japanese and Perl since I
+#   have the free time and the need since II is telecommute
+
+# YAPC::NA
+# - miyagawa's ニート question
+# - cpandoc
+# - announcements
+
+# Study
+# - モダンPerl入門 - vocabulary
+# - CPANモジュールガイド
+# - デューン
+# - This post
+
+# DTrace
+# - new Perl probes
+# - YAPC::Asia (and possibly PPW) talk
 
