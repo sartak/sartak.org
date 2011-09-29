c5ea29b9f4e5ff285e0e8fe89036a8986f778016
Shawn M Moore <sartak@gmail.com>
2011-09-28 22:40:27 -0400

    Actual text

--- writing/why-i-read-books-on-my-ipod.pl
+++ writing/why-i-read-books-on-my-ipod.pl
@@ -9,3 +9,5 @@ hr {};
 h4 { "I can copy Japanese words I _don't_ understand into dictionary:" };
 image "ipod-reading/copy-word.png";
 image "ipod-reading/paste-word.png";
+hr {};
+p { "There are a few reasons why I'm pretty excited about the iPhone 5 announcement. There will supposedly be a fat home button which allows gestures for easy switching of apps, such as between iBooks, Anki, and 大辞林. Also I'm still rocking a third-gen iPod Touch which does not have anything near a retina display, so that'll be a nice upgrade, especially for those muddy kanji." };
