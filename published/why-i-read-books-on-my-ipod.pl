d0a68a08ba3ce7f50e58c590135c71b8d8b0d5e3
Shawn M Moore <sartak@gmail.com>
2011-09-28 23:24:07 -0400

    Footnote about 辞書

--- published/why-i-read-books-on-my-ipod.pl
+++ published/why-i-read-books-on-my-ipod.pl
@@ -7,7 +7,10 @@ image "ipod-reading/copy-sentence.png";
 image "ipod-reading/paste-sentence.png";
 hr {};
 
-h4 { "I can copy Japanese words I _don't_ understand into a dictionary:" };
+h4 {
+    outs "I can copy Japanese words I _don't_ understand into a dictionary:";
+    footnote { "iBooks has a built-in dictionary (the 辞書 button), but it's not useful for my study because it's English-only. One way to make that button universally useful would be to let you set it up to launch some other application, but unfortunately built-in apps typically don't give you that much rope." };
+};
 image "ipod-reading/copy-word.png";
 image "ipod-reading/paste-word.png";
 hr {};
