b85f44f7e4c185357ed182230dd9fc205f53d719
Shawn M Moore <sartak@gmail.com>
2011-09-28 22:42:29 -0400

    More text

--- writing/why-i-read-books-on-my-ipod.pl
+++ writing/why-i-read-books-on-my-ipod.pl
@@ -1,13 +1,17 @@
 use Sartak::Blog;
 
-BEGIN { print "title: Why I Read Books on my iPod\ndraft: 1\n" }
+BEGIN { print "title: Why I Read Books on My iPod\ndraft: 1\n" }
 
 h4 { "I can copy Japanese sentences I understand into my Anki flash card deck:" };
 image "ipod-reading/copy-sentence.png";
 image "ipod-reading/paste-sentence.png";
 hr {};
+
 h4 { "I can copy Japanese words I _don't_ understand into dictionary:" };
 image "ipod-reading/copy-word.png";
 image "ipod-reading/paste-word.png";
 hr {};
-p { "There are a few reasons why I'm pretty excited about the iPhone 5 announcement. There will supposedly be a fat home button which allows gestures for easy switching of apps, such as between iBooks, Anki, and 大辞林. Also I'm still rocking a third-gen iPod Touch which does not have anything near a retina display, so that'll be a nice upgrade, especially for those muddy kanji." };
+
+p { "There are a few reasons why I'm pretty excited about the iPhone 5 announcement. There will supposedly be a fat home button which allows gestures for easy switching of apps, which will save me a couple moments and clicks, thousands of times over, as I flip between iBooks, Anki, and 大辞林 while studying. Also I'm still rocking a third-gen iPod Touch which does not have anything near a retina display, so that'll be for turning those kanji from muddy to crisp." };
+
+p { "I don't foresee myself getting a new Kindle any time soon, even though those Kindle Touches look pretty damn good." };
