ba482784654d0bfa8ca78ca52dad5985dbb6ba59
Shawn M Moore <sartak@gmail.com>
2011-09-28 22:37:28 -0400

    Explanation

--- writing/why-i-read-books-on-my-ipod.pl
+++ writing/why-i-read-books-on-my-ipod.pl
@@ -2,7 +2,12 @@ use Sartak::Blog;
 
 BEGIN { print "title: Why I Read Books on my iPod\ndraft: 1\n" }
 
+h4 { "I can copy Japanese sentences I understand into my Anki flash card deck:" };
 image "ipod-reading/copy-sentence.png";
-image "ipod-reading/copy-word.png";
+br {};
 image "ipod-reading/paste-sentence.png";
+hr {};
+h4 { "I can copy Japanese words I _don't_ understand into dictionary:" };
+image "ipod-reading/copy-word.png";
+br {};
 image "ipod-reading/paste-word.png";
