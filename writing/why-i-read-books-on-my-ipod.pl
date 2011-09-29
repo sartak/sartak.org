ce7d2b532c95675c4ca2c3e4df72038d6889897d
Shawn M Moore <sartak@gmail.com>
2011-09-28 22:37:47 -0400

    No brs

--- writing/why-i-read-books-on-my-ipod.pl
+++ writing/why-i-read-books-on-my-ipod.pl
@@ -4,10 +4,8 @@ BEGIN { print "title: Why I Read Books on my iPod\ndraft: 1\n" }
 
 h4 { "I can copy Japanese sentences I understand into my Anki flash card deck:" };
 image "ipod-reading/copy-sentence.png";
-br {};
 image "ipod-reading/paste-sentence.png";
 hr {};
 h4 { "I can copy Japanese words I _don't_ understand into dictionary:" };
 image "ipod-reading/copy-word.png";
-br {};
 image "ipod-reading/paste-word.png";
