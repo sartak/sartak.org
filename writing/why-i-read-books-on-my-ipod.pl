06cd90f733af2718997af3725a8c88f4c6be94b2
Shawn M Moore <sartak@gmail.com>
2011-09-28 22:36:09 -0400

    New quick article

---
+++ writing/why-i-read-books-on-my-ipod.pl
@@ -0,0 +1,8 @@
+use Sartak::Blog;
+
+BEGIN { print "title: Why I Read Books on my iPod\ndraft: 1\n" }
+
+image "ipod-reading/copy-sentence.png";
+image "ipod-reading/copy-word.png";
+image "ipod-reading/paste-sentence.png";
+image "ipod-reading/paste-word.png";
