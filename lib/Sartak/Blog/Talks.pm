b0f70fb3b9c4eca0d26ecd3f96204bc2155e7292
Shawn M Moore <sartak@gmail.com>
2011-10-23 14:59:08 -0400

    Better descriptions and titles

--- lib/Sartak/Blog/Talks.pm
+++ lib/Sartak/Blog/Talks.pm
@@ -16,7 +16,13 @@ my @talks = (
             url     => 'http://www.perloasis.info/opw2012/',
             talk    => 'http://www.perloasis.info/opw2012/talk/3868',
         },
-        description => q{DTrace is an extremely powerful tool for examining what a computer is doing. Even in production. Without invoking the ire of the Munin Gods. It's also an indispensible tool for programmers who are trying to debug, optimize, or otherwise investigate what their code is doing when it ventures (and venture it must!) outside of the cozy, warm, abstract world of the Perl interpreter.},
+        description => q{DTrace is an extremely powerful tool for examining what a computer is doing. Even in production. Without invoking the ire of the Munin Gods.
+
+    It's also an indispensible tool for programmers who are trying to debug, optimize, or otherwise investigate what their code is doing when it ventures (and venture it must!) outside of the cozy, warm, abstract world of the Perl interpreter.
+
+If you tend towards printf debugging, then DTrace lets you break through to some seriously next-level shit. You don't have to pollute your application's code, or even restart it(!), to begin tracing it. You can also trace the execution of other Perl modules you're using, your operating system kernel, and other applications such as MySQL or Apache. It is awesome!
+
+This talk is intended as an introduction to DTrace for those who have maybe heard that it rocks but haven't ever used it. I'll also describe specifically Perl's support for DTrace and how to make use of the probes we've been adding.},
     },
     {
         name        => 'DTrace: 最強な魔法使いのprintfのようなデバッガ',
@@ -35,7 +41,11 @@ my @talks = (
             url     => 'http://yapcasia.org/2011/',
             talk    => 'http://yapcasia.org/2011/talk/23',
         },
-        description => q{DTrace is an extremely powerful tool for examining what a computer is doing. Even in production. Without invoking the ire of the Munin Gods. It's also an indispensible tool for programmers who are trying to debug, optimize, or otherwise investigate what their code is doing when it ventures (and venture it must!) outside of the cozy, warm, abstract world of the Perl interpreter.},
+        description => q{DTraceは、コンピューターが何を動作していることを調べるために、とても強力なツールです。プロダクションでも使えますよ。ネットワーク監視という神様が怒らず！
+
+    これは、Perlのやさしい世界の厳しい外部でも、デバッグや最適化や監視をするツールです。アプリケーションのコードを汚染していないままで、再起動が必要でなくて、トレースを示す可能があります！しかも、他の使っているモジューでも、カーネルプロセスでも、他のアプリケーションル（例えば:MySQL、Apache）でも、トレースで動作を検査できます。printfのようなデバッグすることが好きの方にとって、DTraceも好きになると思います。
+
+このトークでは、「DTraceはすごすぎるよ！」と聞いたが使ったことがない方のためにDTrace入門です。それ以外で、PerlのDTraceのサポートと、Perlに追加したプローブを説明します。},
     },
     {
         name        => 'Announcing <i>Announcements</i>',
@@ -53,7 +63,11 @@ my @talks = (
             url     => 'http://www.yapc2011.us/yn2011',
             talk    => 'http://www.yapc2011.us/yn2011/talk/3197',
         },
-        description => q{Say you&#39;re writing a system that uses the observer design pattern. What happens if instead of passing around the NAME of an event, you pass around an OBJECT representing the event? Turns out, a whole lot of goodness! Announcements enable communication from the observer to the observed, and even communication &#40;and conspiracy&#41; amongst observers themselves. All with a very strongly object-oriented, antler-shaped flavor.},
+        description => q{Say you're writing a system that uses the observer design pattern. What happens if instead of passing around the NAME of an event, you pass around an OBJECT representing the event? Turns out, a whole lot of goodness!
+
+    Announcements enable communication from the observer to the observed, and even communication (and conspiracy) amongst observers themselves.
+
+All with a very strongly object-oriented, antler-shaped flavor.},
     },
     {
         name        => 'cpandoc',
@@ -89,7 +103,7 @@ my @talks = (
             url     => 'http://yapcasia.org/2010/',
             talk    => 'http://yapcasia.org/2010/talks/63D22246-BC8C-11DF-8791-B9FC0F276C45',
         },
-        description => q{A new-and-improved version of my YAPC::NA talk directly below. This talk has become a case study in Moosey design, describing how the needs of Path::Dispatcher&#39;s users have influenced its evolution. Too many talks present a topic as though the solution sprung fully-formed from the designer&#39;s brain in an instant, ignoring the interesting details of how a system is improved over time. Maybe next conference I&#39;ll present The Evolution of &#39;The Evolution of Path::Dispatcher&#39;, a talk on how to evolve talks after presenting them. :&#41;},
+        description => q{A new-and-improved version of my YAPC::NA talk directly below. This talk has become a case study in Moosey design, describing how the needs of Path::Dispatcher's users have influenced its evolution. Too many talks present a topic as though the solution sprung fully-formed from the designer's brain in an instant, ignoring the interesting details of how a system is improved over time. Maybe next conference I'll present The Evolution of "The Evolution of Path::Dispatcher", a talk on how to evolve talks after presenting them. :)},
     },
     {
         name        => 'Path::Dispatcher',
@@ -107,7 +121,9 @@ my @talks = (
             url     => 'http://yapc2010.com/yn2010/',
             talk    => 'http://yapc2010.com/yn2010/talk/2642',
         },
-        description => q{This talk will give a brief overview of Path::Dispatcher&#39;s features. Then the meat of the talk will describe how its flexible design has met the challenges thrown at it, and how Path::Dispatcher&#39;s sibling rival, Path::Router, would have fared.},
+        description => q{Path::Dispatcher solves the problem of "dispatch" - mapping a string (the path) through a set of rules to find matches and then act upon them. Most commonly this is done in web applications to run one or more actions based on each incoming request URI. However, Path::Dispatcher, unlike most of the other modules in its niche, is not married to the web. It is also used to dispatch based on command-line arguments in some applications, and to dispatch commands in a MUD engine.
+
+    This talk gives a brief overview of Path::Dispatcher's features. Then the meat of the talk describes how its flexible design has met the challenges thrown at it, and how Path::Dispatcher's sibling rival, Path::Router, would have fared.},
     },
     {
         name        => 'Inline::C',
@@ -124,7 +140,7 @@ my @talks = (
             dir     => 'tsukuba.xs-1',
             url     => 'http://atnd.org/events/4628',
         },
-        description => q{A 5-minute introduction to Inline::C. In Japanese! I wrote the talk in English and then translated it, so English speakers will be able to read it too. :&#41;},
+        description => q{A 5-minute introduction to Inline::C. In Japanese! I wrote the talk in English and then translated it, so English speakers will be able to read it too. :)},
     },
     {
         name        => 'Nonhierarchical OOP',
@@ -141,7 +157,7 @@ my @talks = (
             dir     => 'osdc.tw-2010',
             url     => 'http://osdc.tw/',
         },
-        description => q{The current trend for new programming languages seems to be to include more and more nonhierarchical OOP features. Some examples are new-style interfaces in Google&#39;s Go, traits in Scala and Fortress, and roles in Perl 6. This talk will focus on roles as provided by Perl 5&#39;s Moose object system, as that seems to have the most uptake and is most relevant for OSDC.tw&#39;s audience. Furthermore, because of roles&#39; &#34;class building-block&#34; nature, they are perhaps the best fit for open source of any nonhierarchical system thus far.},
+        description => q{The current trend for new programming languages seems to be to include more and more nonhierarchical OOP features. Some examples are new-style interfaces in Google's Go, traits in Scala and Fortress, and roles in Perl 6. This talk will focus on roles as provided by Perl 5's Moose object system, as that seems to have the most uptake and is most relevant for OSDC.tw's audience. Furthermore, because of roles' "class building-block" nature, they are perhaps the best fit for open source of any nonhierarchical system thus far.},
     },
     {
         name        => 'Surviving in the Cruel, Unforgiving World',
@@ -156,7 +172,7 @@ my @talks = (
             url     => 'http://www.perloasis.info/opw2010/',
             talk    => 'http://www.perloasis.info/opw2010/talk/2476',
         },
-        description => q{This talk about my NetHack-playing AI was very informal and impromptu. I blathered on about <a href="http://taeb.sartak.org">TAEB</a>&#39;s design, mostly about making proper use of object-oriented design, while TAEB played NetHack on the projector. If I give this talk again I will definitely write slides because there is a lot of good design in TAEB. Without slides I think it bored the people who didn&#39;t know much about NetHack. On the other hand, it <i>enchanted</i> those who did.},
+        description => q{This talk about my NetHack-playing AI was very informal and impromptu. I blathered on about <a href="http://taeb.sartak.org">TAEB</a>'s design, mostly about making proper use of object-oriented design, while TAEB played NetHack on the projector. If I give this talk again I will definitely write slides because there is a lot of good design in TAEB. Without slides I think it bored the people who didn't know much about NetHack. On the other hand, it <i>enchanted</i> those who did.},
     },
     {
         name        => '(Parameterized) Roles',
@@ -175,7 +191,15 @@ my @talks = (
             url     => 'http://conferences.yapcasia.org/ya2009/',
             talk    => 'http://conferences.yapcasia.org/ya2009/talk/2261',
         },
-        description => q{Roles are an excellent object-oriented tool both for allomorphism and for reuse. Roles can combine with conflict detection. This eliminates accidental shadowing of methods that is painful with multiple inheritance and mixins. Parameterized roles &#40;via MooseX::Role::Parameterized&#41; improve the reusability of roles by letting each consumer cater the role to its needs.},
+        description => q{Roles are an excellent object-oriented tool both for allomorphism and for reuse.
+
+    Roles facilitate allomorphism by favoring "does this object do X" versus "is this object a subclass of X". You often care more about capability than inheritance. In a sense, roles encode types better than inheritance.
+
+Roles also provide an excellent faculty for reuse. This effectively eliminates multiple inheritance, which is often the only solution for sharing code between unrelated classes.
+
+Roles can combine with conflict detection. This eliminates accidental shadowing of methods that is painful with multiple inheritance and mixins.
+
+Parameterized roles (via MooseX::Role::Parameterized) improve the reusability of roles by letting each consumer cater the role to its needs. This does sacrifice some allomorphism, but there are ways to restore it.},
     },
     {
         name        => 'API Design',
@@ -194,7 +218,13 @@ my @talks = (
             url     => 'http://conferences.yapcasia.org/ya2009/',
             talk    => 'http://conferences.yapcasia.org/ya2009/talk/2188',
         },
-        description => q{Too few projects demand good API design as a critical goal. A clean and extensible API will pay for itself many times over in fostering a community of plugins. We certainly cannot anticipate the ways in which our users will bend our modules, but designing an extensible system alleviates the pain. There are many lessons to be learned from Moose, HTTP::Engine and IM::Engine, Dist::Zilla, KiokuDB, Fey, and TAEB.},
+        description => q{Too few projects demand good API design as a critical goal. A clean and extensible API will pay for itself many times over in fostering a community of plugins. We certainly cannot anticipate the ways in which our users will bend our modules, but designing an extensible system alleviates the pain. There are many lessons to be learned from Moose, HTTP::Engine and IM::Engine, Dist::Zilla, KiokuDB, Fey, and TAEB.
+
+    The most important lesson is to decouple the core functionality from the "fluff" such as sugar and middleware. This forces you to have a solid API that ordinary users can extend. This also lets users write their own sugar and middleware. In a tightly-coupled system, there is little hope for extensibility.
+
+In this talk, you will learn how to make very productive use of Moose's roles to form the foundation of a pluggable system. Roles provide excellent means of code reuse and safe composition. I will also demonstrate how to use Sub::Exporter to construct a more useful and flexible sugar layer.
+
+Finally, I will reveal the secret to designing excellent APIs.},
     },
     {
         name        => 'Extending Moose for Applications',
@@ -212,7 +242,13 @@ my @talks = (
             url     => 'http://yapc10.org/yn2009/',
             talk    => 'http://yapc10.org/yn2009/talk/1880',
         },
-        description => q{The real power of Moose rests in its extensibility. By subclassing Moose&#39;s metaclasses, you can augment and change Moose&#39;s behavior to suit your application&#39;s needs. This extensibility is powered by the meta-object protocol of Moose&#39;s foundation, Class::MOP. This talk focuses on domain-specific metaprogramming, particularly with traits.},
+        description => q{Using Moose provides many immediate and obvious benefits, starting with the obviation of typing "use strict" and "use warnings" in your classes.
+
+    The real power of Moose, however, rests in its extensibility. By subclassing Moose's metaclasses, you can augment and change Moose's behavior to suit your application's needs. This extensibility is powered by the meta-object protocol of Moose's foundation, Class::MOP.
+
+The motivating example for extending Moose will be the creation of a small web framework to empower a Twitter-alike. The focus will be creating meta-level roles so that metaclasses may select exactly which changes in behavior they wish to exhibit. Modules that will be used include Moose::Exporter (to define sugary keywords) and Moose::Util::MetaRole (to extend classes composably).
+
+Experience with using Moose to create regular classes is expected. Having some familiarity with roles will let you get more out of the talk. No experience with metaprogramming is required.},
     },
     {
         name        => 'Introduction to Moose',
@@ -226,10 +262,10 @@ my @talks = (
             url     => 'http://www.frozen-perl.org/mpw2009/',
             talk    => 'http://www.frozen-perl.org/mpw2009/talk/1687',
         },
-        description => q{Yet another introductory Moose talk. What separates this from other Moose Intro talks is that it focuses entirely on the immediate time-saving benefits of Moose. No mention of Class::MOP &#40;surely an advanced topic&#41; is made. This talk covers most options of &#39;has&#39;, method modifiers, and roles.},
+        description => q{Yet another introductory Moose talk. What separates this from other Moose Intro talks is that it focuses entirely on the immediate time-saving benefits of Moose. No mention of Class::MOP (surely an advanced topic) is made. This talk covers most options of "has", method modifiers, and roles.},
     },
     {
-        name        => 'Devel::REPL',
+        name        => 'Devel::REPL and Carp::REPL',
         skip_index  => 1,
         dir         => 'devel-repl',
         length      => '45 min',
@@ -240,7 +276,11 @@ my @talks = (
             url     => 'http://www.frozen-perl.org/mpw2009/',
             talk    => 'http://www.frozen-perl.org/mpw2009/talk/1663',
         },
-        description => q{Devel::REPL is an excellent addition to your toolkit. It is an interactive shell for Perl; you type in a line of Perl code and Devel::REPL will evaluate it. This talk introduces Devel::REPL and Carp::REPL. It then delves into the design of Devel::REPL, and how its strong use of roles cleanly supports extensibility.},
+        description => q{Devel::REPL is an excellent addition to your toolkit. It is an interactive shell for Perl; you type in a line of Perl code and Devel::REPL will evaluate it. You can use Devel::REPL to quickly test out code or to provide a debugging shell for your applications. There are many plugins, such as simply adding color, nopasting your session to a pastebin, and even dumping each line of code with B::Concise.
+
+Carp::REPL builds on top of Devel::REPL to make finding bugs easier. By default, when an exception is thrown, you get a shell (though you can tweak this to get a shell on any warning or test failure). You can examine and change variables, call and redefine functions, and anything else you can do with real Perl code. Carp::REPL lets you have a shell "inside" your applications with *zero* code changes required.
+
+Matt Trout's design for Devel::REPL is worthy of study. It is a Moose library that makes heavy use of roles to provide a great deal of customization for users. Sans plugins, Devel::REPL is just about the simplest implementation of a shell, but that just means it has the greatest amount of flexibility. This final part of the talk will be case study in the design and implementation of a nontrivial Moose application.},
     },
     {
         name        => 'Template::Declare',
@@ -254,12 +294,13 @@ my @talks = (
             url     => 'http://www.frozen-perl.org/mpw2008/',
             talk    => 'http://www.frozen-perl.org/mpw2008/talk/858',
         },
-        description => q{Template::Declare is a novel templating system written in Perl. &#39;TD&#39; has a very pleasant block-oriented syntax. This talk introduces its features and shortcomings, then describes how it is implemented.},
+        description => q{Template::Declare is a novel templating system written in Perl. "TD" has a very pleasant block-oriented syntax. This talk introduces its features and shortcomings, then describes how it is implemented.},
     },
 );
 
 for (@talks) {
     $_->{url} = "http://sartak.org/talks/$_->{conference}{dir}/$_->{dir}/";
+    $_->{description} =~ s{([^\n]+)}{<p>$1</p>}g;
 }
 
 sub talks { @talks }
