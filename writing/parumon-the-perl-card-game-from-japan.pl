caf7e0401cef8734a456407b6d24229ab80a8bd7
Shawn M Moore <sartak@gmail.com>
2011-10-30 10:56:35 -0400

    Start writing Parumon

---
+++ writing/parumon-the-perl-card-game-from-japan.pl
@@ -0,0 +1,61 @@
+use Sartak::Blog;
+
+BEGIN { print "title: Parumon: The Perl Card Game from Japan\ndraft: 1\n" }
+
+p { "At YAPC::Asia, [\@maka2_donzoko](http://twitter.com/maka2_donzoko) released [Parumon: The Perl Monger Training Card Game](http://donzoko.net/parumon/). As soon as I heard about it I quickly snatched one up (which was fortunate since they of course sold out on the first day). I don't intend to give a full translation of all the rules and cards, but I'd like to give you an idea of how it works." };
+
+p { "For ego surfers here's the list of modules that appear in the game: " };
+
+ul {
+    my @modules = map { [ /(\W+)(?: (\dx))?/ ] } split /\n/, '
+        Acme::Bleach
+        Amon2
+        AnyEvent
+        Catalyst::Runtime
+        DBI x2
+        DBIx::Class
+        DBIx::Skinny
+        DateTime
+        Devel::Cover
+        Email::Send
+        Email::Sender
+        HTML::Template
+        Log::Handler
+        Log::Minimal
+        MIME::Lite
+        MetaCPAN::API
+        Mojolicious
+        Moose
+        Mouse x2
+        OrePAN
+        POE
+        Path::Class x2
+        Plack
+        Template-Toolkit
+        Teng
+        Test::Base
+        Test::More x2
+        Test::Perl::Critic
+        Test::Pod
+        Text::MicroTemplate
+        Text::Xslate
+        Time::Piece x2
+        Try::Tiny x3
+        URI x2
+    ';
+
+    for (@modules) {
+        my ($module, $note) = @$_;
+        li {
+            a {
+                href is "http://metacpan.org/module/$module";
+                $module
+            };
+
+            if ($note) {
+                outs " $note";
+            }
+        }
+    }
+}
+
