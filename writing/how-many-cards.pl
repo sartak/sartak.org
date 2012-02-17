964c3c9a733e6e7dd9dcd417a25c39a3701a037d
Shawn M Moore <sartak@gmail.com>
2012-02-16 22:48:41 -0500

    Conclusion

--- writing/how-many-cards.pl
+++ writing/how-many-cards.pl
@@ -4,7 +4,7 @@ BEGIN { print "title: How Many Sentences Teaches a Word?\ndraft: 1\n" }
 
 p { "One of the questions I wonder about is how many sentences I need to add to my flash card deck to learn a particular word or kanji well enough to have no trouble with it. There are certainly a lot of tricky words such as 敢行 (decisive action) and 格納 (store away) that always seem to slip away from me. I suspect that is entirely because I have one flash card each for those words. I just don't have any other comprehensible sentences for them in the corpus of text I'm accumulating. Because I need to see more of those words to get them to stick in my head, should I just give up and delete those two important sentences from my deck? Typically what I'll do instead, which has worked well for words like 薄皮 (thin covering film) and 七面鳥 (turkey), is to search Twitter. Even for the most obscure words I come across, Twitter gives me thousands of short example sentences." };
 
-p { "I'm certain that at my skill level, one card is not enough to cement a completely new word in my passive vocabulary. Is two cards enough? Maybe. I'm sure five is sufficient. But if I can't get five sentences I don't hate, should I skip the word?" };
+p { "I'm certain that at my skill level, one card is not always enough to cement a completely new word in my passive vocabulary. Is two cards enough? Maybe. I'm sure five is generally sufficient. But if I can't get five sentences I don't hate, should I skip the word?" };
 
 p { "Luckily to answer such questions, I have a giant (currently 14,722 flash card) list of Japanese sentences that I can mostly comprehend. Each sentence even include a set of statistics including how many times I've reviewed it, what my breakdown of incorrect versus correct responses were, when I first reviewed it, and so on. Since I'm very diligent about learning using this flash card system, **I can data mine my understanding of Japanese.**" };
 
@@ -42,6 +42,8 @@ div {
 };
 
 p { "This means if I add one card for the word, I've got a decent 87% chance of recalling it on time. A second card helps a _lot_. Add a few more cards and my chances are golden." };
+
+p { "I should search Twitter for tweets mentioning tricky words like 格納 and 敢行, add a few cards, and never have problems with them again." };
 
 h3 { "English" };
 
