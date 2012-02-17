903e8a90d7dac58140083189e3aa56c3af09dd77
Shawn M Moore <sartak@gmail.com>
2012-02-16 22:18:37 -0500

    Add progression

--- writing/how-many-cards.pl
+++ writing/how-many-cards.pl
@@ -4,11 +4,23 @@ BEGIN { print "title: How Many Flash Cards Do I Need?\ndraft: 1\n" }
 
 p { "One of the questions I wonder about is how many sentences I need to add to my flash card deck to learn a particular word or kanji well enough to have no trouble with it. There are certainly a lot of tricky words such as 敢行 (decisive action) and 格納 (store away) that always seem to slip away from me. I suspect that is entirely because I have one flash card each for those words. I just don't have any other comprehensible sentences for them in the corpus of text I'm accumulating. Because I need to see more of those words to get them to stick in my head, should I just give up and delete those two important sentences from my deck? Typically what I'll do instead, which has worked well for words like 薄皮 (thin covering film) and 七面鳥 (turkey), is to search Twitter. Even for the most obscure words I come across, Twitter gives me thousands of short example sentences." };
 
-p { "I'm certain that at my skill level, one card is not enough to cement a completely new word in my passive vocabulary. Is two cards enough? I'm fairly sure five is sufficient. But if I can't get five sentences I don't hate, should I skip the word?" };
+p { "I'm certain that at my skill level, one card is not enough to cement a completely new word in my passive vocabulary. Is two cards enough? Maybe. I'm sure five is sufficient. But if I can't get five sentences I don't hate, should I skip the word?" };
 
 p { "Luckily to answer such questions, I have a giant (currently 14,722 flash card) list of Japanese sentences that I can mostly comprehend. Each sentence even include a set of statistics including how many times I've reviewed it, what my breakdown of incorrect versus correct responses were, when I first reviewed it, and so on. Since I'm very diligent about learning using this flash card system, **I can data mine my understanding of Japanese.**" };
 
 p { "Using [Text::MeCab] to split up Japanese sentences (which do not use spaces) into stemmed words, I can get visibility into how hard each word itself is, based on my review history of the sentence cards which contain it. Since I do a fair amount of this kind of analysis anyway, I already had most of the tools I needed. 50 lines of Perl later I have this data set:" };
+
+pre {
+    outs_raw join "\n", (
+        '1: 87.75%',
+        '2: 91.19%',
+        '3: 92.59%',
+        '4: 93.36%',
+        '5: 95.06%',
+        '6: 95.80%',
+        '7: 96.38%',
+    );
+};
 
 h3 { "English" };
 
