4705a9e529f84dafb4ecf13991273516d7df2a63
Shawn M Moore <sartak@gmail.com>
2012-02-26 16:06:20 -0500

    Links

--- writing/im-published.pl
+++ writing/im-published.pl
@@ -2,5 +2,6 @@ use Sartak::Blog;
 
 BEGIN { print "title: I'm Published!\ndraft: 1\n" }
 
-p { "Today I received a complimentary copy of Web+DB Press volume 67. My article, メタオブジェクトプロトコル入門, <i>Introduction to Metaobject Protocols</i>, is the thirteenth entry in the article series [Perl Hackers Hub](http://gihyo.jp/dev/serial/01/perl-hackers-hub)." };
+p { "Today I received a complimentary copy of [WEB+DB Press volume 67](http://www.amazon.co.jp/WEB-DB-PRESS-Vol-67-%E5%B7%9D%E5%8F%A3/dp/4774149942/ref=sr_1_1?ie=UTF8&qid=1330290184&sr=8-1), which is a Japanese magazine about programming. My article, メタオブジェクトプロトコル入門, <i>Introduction to Metaobject Protocols</i>, is the thirteenth entry in the series of articles called [Perl Hackers Hub](http://gihyo.jp/dev/serial/01/perl-hackers-hub)." };
 
+# http://www.amazon.co.jp/s/ref=ntt_athr_dp_sr_7?_encoding=UTF8&search-alias=books-jp&field-author=Shawn%20M%20Moore
