ffb176b991034ae4683cd4c47789542e2f858f5e
Shawn M Moore <sartak@gmail.com>
2012-02-26 16:02:27 -0500

    Start writing I'm Published!

---
+++ writing/im-published.pl
@@ -0,0 +1,6 @@
+use Sartak::Blog;
+
+BEGIN { print "title: I'm Published!\ndraft: 1\n" }
+
+p { "Today I received a complimentary copy of Web+DB Press volume 67. My article, メタオブジェクトプロトコル入門, <i>Introduction to Metaobject Protocols</i>, is the thirteenth entry in the article series [Perl Hackers Hub](http://gihyo.jp/dev/serial/01/perl-hackers-hub)." };
+
