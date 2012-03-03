0c711e9e8b891dd6ed25ce15c0a9b3d5dac506fd
Shawn M Moore <sartak@gmail.com>
2012-03-02 19:44:13 -0500

    Better commit haha

--- writing/near-miss-words.pl
+++ writing/near-miss-words.pl
@@ -2,7 +2,7 @@ use Sartak::Blog;
 
 BEGIN { print "title: Near-Miss Words\ndraft: 1\n" }
 
-p { "I do a fair amount of morphological analysis of my corpus of Japanese content. Today I thought up and [quickly implemented](https://github.com/sartak/anki-bin/commit/1dacb4fcf2) a new way to order words I should learn next. I call them *near-miss words* because each one is the single word that I don't understand in an otherwise comprehensible sentence. In other words, they are the missing \"+1\" in [i+1 sentences](http://en.wikipedia.org/wiki/Input_Hypothesis). Learning near-miss words could be a productive way to fill gaps in my understanding." };
+p { "I do a fair amount of morphological analysis of my corpus of Japanese content. Today I thought up and [quickly implemented](https://github.com/sartak/anki-bin/commit/33ded524) a new way to order words I should learn next. I call them *near-miss words* because each one is the single word that I don't understand in an otherwise comprehensible sentence. In other words, they are the missing \"+1\" in [i+1 sentences](http://en.wikipedia.org/wiki/Input_Hypothesis). Learning near-miss words could be a productive way to fill gaps in my understanding." };
 
 p { "I have a lot of modules and scripts written in Perl to help me with this, but the most important chunk of this is [mecab](http://mecab.googlecode.com/). Mecab breaks up Japanese sentences into stemmed (i.e. normalized) words. Without it I would have nothing for analyzing what I do and do not know." };
 
