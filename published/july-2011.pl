b6c8ce1d052b1c59650b5926482b4f19b67cb4b7
Shawn M Moore <sartak@gmail.com>
2012-04-15 20:50:29 -0400

    july-2011

--- published/july-2011.pl
+++ published/july-2011.pl
@@ -4,5 +4,5 @@ BEGIN { print "title: July 2011\ndate: 2011-08-01\n" }
 
 p { "A telling side effect of my month of doing (mostly) nothing but studying Japanese:" };
 
-img { src is 'http://sartak.org/misc/blog/july2011.png' };
+img { src is '/img/blog/july-2011/new-cards.png' };
 
