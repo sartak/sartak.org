7ec8c5b39e416f448b4ec8b19d9896525e8ef7c8
Shawn M Moore <code@sartak.org>
2012-08-04 20:25:42 -0400

    Why Dragon Quest Never Took Off in America

---
+++ published/why-dragon-quest-never-took-off-in-america.pl
@@ -0,0 +1,12 @@
+use Sartak::Blog;
+
+BEGIN { print "title: Why Dragon Quest Never Took Off in America\ndate: 2012-08-04\n" }
+
+p { "This is the _first_ dungeon." };
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7713651052/";
+    image "why-dragon-quest-never-took-off-in-america/maze-1.jpg";
+};
+
+p { "I think I'm going to love this game." };
