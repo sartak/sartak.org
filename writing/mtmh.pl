f8ea2fbaeca30fd2b5beafa503962fb69e235f15
Shawn M Moore <code@sartak.org>
2012-08-28 16:38:16 +0200

    Photos!

--- writing/mtmh.pl
+++ writing/mtmh.pl
@@ -8,16 +8,91 @@ p { "会議のテーマは二つありました。1つは[RDF](http://act.yapc.e
 h3 { "Day -3 (YAPC::EUの日)" };
 p { "ドイツのフランクフルトで開催されたYAPC::EUに参加できました。初めてYAPC::EUだし、イギリス以外ヨーロッパに行ったことがなかったから、とても嬉しいでした。YAPCで「[Mooseロール利用パターン](http://sartak.org/talks/yapc-eu-2012/role-usage-patterns/)」というトークをやっていきました。私が今回は全ての前回よりよくできたな！と思います。きっと来年のYAPC::Asiaに行くことがてきるので、たぶん日本語版を話そうとします。" };
 
+a {
+    href is "http://www.flickr.com/photos/sartak/7838221830";
+    image "mtmh/yapc-audience.jpg";
+};
+
+
 h3 { "Day 0 (旅の日)" };
 p { "フランクフルトからスタヴァンゲルに飛んで行きました。ホテルからフランクフルト空港に、そして飛行機に通った道が分かりにくいと思います。中央(Hauptbahnhof)というか駅で乗り換えの標識がちょっと読めませんでした。しかも、空港のゲートを越えた後、バス(!)で飛行機に運ばされまして、道路から階段で直接に乗りました。ビックリした！" };
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880429950";
+    image "mtmh/flight-in.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880429316";
+    image "mtmh/oslo-bus.jpg";
+};
+
 p { "結局、プレーケストーレンの山小屋に無事に到着しました。" };
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880431538";
+    image "mtmh/lodge-lake.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880431082";
+    image "mtmh/lodge-interior.jpg";
+};
+
 p { "参加者の皆さんが、自己紹介したり、喋ったり、一緒に夕食を食べました。p5mopに興味ある参加者が「基本的に、どうやってroleという概念はp5mopの仕組みに合う？」というを夜中すぎまで論じました。" };
 p { "[\@robinsmidsrod](http://twitter.com/robinsmidsrod)さんが「日本語を入力する方法を説明しませんか？」と聞いて、私はもちろん嬉しくなりました ;) その後、XPathとか[XML::Rabbit]とか[Unicode CLDR](http://ja.wikipedia.org/wiki/Common_Locale_Data_Repository)とかとか喋りました。" };
 
 h3 { "Day 1 (登山の日)" };
 p { "早起きして、プレーケストーレンという有名な崖にハイキングしました。眺めはとっっってもきれいでした！！！" };
 p { "旅立ちは10時から16時までかかりました。たいへん疲れました。（三日後でもまだ足が痛い！）" };
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880432218";
+    image "mtmh/picnic.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880432884";
+    image "mtmh/rocky-forest.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880376022";
+    image "mtmh/wave-of-trees.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880377090";
+    image "mtmh/focused-cliff.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880435618";
+    image "mtmh/pulpit-rock.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880377758";
+    image "mtmh/topdown-cliff.jpg";
+};
+
+
 p { "その後で、Fluxx(zombie版とcthulhu版組み合わせられた)というカードゲームをやりました。計画者はプログラマーですから、法則は面白くて、進化することまであります。" };
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880437426";
+    image "mtmh/stevan-peek.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880438210";
+    image "mtmh/fluxx-rules.jpg";
+};
+
+a {
+    href is "http://www.flickr.com/photos/sartak/7880438674";
+    image "mtmh/pro-perigrin.jpg";
+};
 
 h3 { "Day 2 (ハッカソンの一日目)" };
 p { "朝食から昼食まで私が「Mooseロール利用パターン」というトーク拡張版を話してあげました。昼食の後で、[\@pmichaud](http://twitter.com/pmichaud)さんが[NQP言語](http://pmichaud.com/2012/pres/mtmh2012-nqp/slides/start.html)について解説しました。プログラミング言語が作りたい方は、きっとNQPでとても早く計画できると思います。しかもハッカソンの間で[\@pmichaud](http://twitter.com/pmichaud)さんが「ちゃんとドキュメンテーションが書きたい」と言いました。" };
@@ -25,9 +100,14 @@ p { "[\@perlyarg](http://twitter.com/perlyarg)さんとMooseのロールテス�
 p { "最初のp5mopの拡張として[mopx::instance::tracking](https://github.com/stevan/p5-mop/commit/04997c0c93c7)は、私が書いたことができました。とても簡単に、p5mopのClassというメタクラスをサブクラスして、create_instanceというメソッドにオブジェクトを見逃さない挙動を追加されます。" };
 p { "夕飯の後で、昨日の旅だし、朝のトークをしたから、たいへん疲れました。で、やったことはFluxxだけでした。" };
 
+a {
+    href is "http://www.flickr.com/photos/sartak/7879978022";
+    image "mtmh/perigrin-workstation.jpg";
+};
+
 h3 { "Day 3 (ハッカソンの二日目)" };
 p { "先日書いたDTraceプローブをPerlに追加する[パッチ](https://rt.perl.org/rt3/Ticket/Display.html?id=114638)をやっと送りました。もし受け入れたら、5.18からop-entry、loading-file、loaded-fileというプローブを使うようになります。" };
-p { "誰かが「正規表現を使いこなす教師いますかー！」と聞いて、「私が手伝ってみてあげましょう」と私は答えました。問題は、テストが9秒以上で実行するので、ちょっと遅すぎました。役800回呼び出したregcomp(正規表現をコンパイルする関数)というopcodeに2秒以上部分があることを[Devel::NYTProf]で見つけました。でも、ちゃんと`qr//`を使ったので、regcompがほとんど呼ばないはずだと思いました。残念ですがDevel::NYTProfがopcode内に見ることができませんが、DTraceは各C関数でも見られます。そして私がperl providerのsub-entryとsub-returnというプローブをトレースして、pid providerのC関数のentryとreturnで、遅いマッチだけ内regcompをトレースしました。発見として、700部分のregcompは速いですけど、あと100部分のregcompはその速いregcompより100x以上遅い。なに。。。？後程、遅すぎた正規表現は記録括弧がありまして、その括弧を`(?:...)`に変更することで、とてみ速くなったことができました！実行は9秒から2秒まで減りました。" };
+p { "Rubenさんkが「正規表現を使いこなす教師いますかー！」と聞いて、「私が手伝ってみてあげましょう」と私は答えました。問題は、テストが9秒以上で実行するので、ちょっと遅すぎました。役800回呼び出したregcomp(正規表現をコンパイルする関数)というopcodeに2秒以上部分があることを[Devel::NYTProf]で見つけました。でも、ちゃんと`qr//`を使ったので、regcompがほとんど呼ばないはずだと思いました。残念ですがDevel::NYTProfがopcode内に見ることができませんが、DTraceは各C関数でも見られます。そして私がperl providerのsub-entryとsub-returnというプローブをトレースして、pid providerのC関数のentryとreturnで、遅いマッチだけ内regcompをトレースしました。発見として、700部分のregcompは速いですけど、あと100部分のregcompはその速いregcompより100x以上遅い。なに。。。？後程、遅すぎた正規表現は記録括弧がありまして、その括弧を`(?:...)`に変更することで、とてみ速くなったことができました！実行は9秒から2秒まで減りました。" };
 p { "AUTOLOADを使っているモジュールには、「URI::Namespace=HASH(0x7ff5fa032360) contains invalid characters for a type name. Names can contain alphanumeric character, \":\", and \".\"」というエラーが発生していました。結局、原因は`\$obj->type`呼び出して、AUTOLOADを実行するはずがあるけど、実は名前として`\$obj`を渡して、タイプを定義する`Moose::Util::TypeConstraint::type(\$obj)`を呼び出されました。`no Moose::Util::TypeConstraint`とか`use namespace::autoclean`を追加すると、typeというメソッドはAUTOLOADを実行するので、よく解決しました。" };
 
 h3 { "Day 4 (ハッカソンの三日目)" };
