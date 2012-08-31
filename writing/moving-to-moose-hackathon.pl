83b624a192c3dae8e100a37a9d9cee08c8ab0170
Shawn M Moore <code@sartak.org>
2012-08-31 16:17:51 -0400

    Editing, finishing up. Just need more photos now

--- writing/moving-to-moose-hackathon.pl
+++ writing/moving-to-moose-hackathon.pl
@@ -3,10 +3,10 @@ use Sartak::Blog;
 BEGIN { print "title: Mooseを利用するに変えるハッカソン\nbasename: moving-to-moose-hackathon\ndraft: 1\n" }
 
 p { "Oslo.pmが「[Moving to Moose Hackathon](http://act.yapc.eu/mtmh2012/)(Mooseを利用するに変えるハッカソン)」という会議をノルウェーのプレーケストーレンにある山小屋で開催しました。私は参加したいでしたが、招待頂きました。Oslo.pm、どうもありがとうございます！" };
-p { "会議のテーマは二つありました。1つは[RDF](http://act.yapc.eu/mtmh2012/sponsors.html#perlrdf)に興味あるコミュニティが、Mooseを利用するにモジュールを変えるつもりでした。あと1つは、[p5mop](http://act.yapc.eu/mtmh2012/sponsors.html#p5mop)を屈強化して、もう少しperl自体に追加できる状態に準備する予定がありました。" };
+p { "会議のテーマは二つありました。1つは[RDF](http://act.yapc.eu/mtmh2012/sponsors.html#perlrdf)に興味あるコミュニティが、Mooseを利用するにモジュールを変えるつもりでした。あと1つは、[p5mop](http://act.yapc.eu/mtmh2012/sponsors.html#p5mop)を屈強化して、もう少しperl自体に追加できる状態に準備する予定たちました。" };
 
 h3 { "Day -3 (YAPC::EUの日)" };
-p { "ドイツのフランクフルトで開催されたYAPC::EUに参加できました。初めてYAPC::EUだし、イギリス以外ヨーロッパに行ったことがなかったから、とても嬉しいでした。YAPCで「[Mooseロール利用パターン](http://sartak.org/talks/yapc-eu-2012/role-usage-patterns/)」というトークをやっていきました。私が今回は全ての前回よりよくできたな！と思います。きっと来年のYAPC::Asiaに行くことがてきるので、たぶん日本語版を話そうとします。" };
+p { "ドイツのフランクフルトで開催されたYAPC::EUに参加できました。初めてYAPC::EUだったし、イギリス以外ヨーロッパに行ったことがなかったから、とても嬉しいでした。YAPCで「[Mooseロール利用パターン](http://sartak.org/talks/yapc-eu-2012/role-usage-patterns/)」というトークをやっていきました。今回のトークは、全ての前回よりよくできたな！と私は思います。きっと来年のYAPC::Asiaに行くことがてきるので、たぶん日本語版を話そうとします。今年来られないのはすみません！" };
 
 a {
     href is "http://www.flickr.com/photos/sartak/7838221830";
@@ -15,7 +15,7 @@ a {
 
 
 h3 { "Day 0 (旅の日)" };
-p { "フランクフルトからスタヴァンゲルに飛んで行きました。ホテルからフランクフルト空港に、そして飛行機に通った道が分かりにくいと思います。中央(Hauptbahnhof)というか駅で乗り換えの標識がちょっと読めませんでした。しかも、空港のゲートを越えた後、バス(!)で飛行機に運ばされまして、道路から階段で直接に乗りました。ビックリした！" };
+p { "フランクフルトからスタヴァンゲルに飛んで行きました。ホテルからフランクフルト空港に、そして飛行機に通った道が分かりにくいと思いました。中央(Hauptbahnhof)というか駅で乗り換えの標識がちょっと読めませんでした。ドイツ語は全然分からないのでした。しかも、空港のゲートを越えた後、バス(!)で飛行機に運ばされまして、道路から階段で直接に乗りました。ビックリした！" };
 
 a {
     href is "http://www.flickr.com/photos/sartak/7880429950";
@@ -40,11 +40,11 @@ a {
 };
 
 p { "参加者の皆さんが、自己紹介したり、喋ったり、一緒に夕食を食べました。p5mopに興味ある参加者が「基本的に、どうやってroleという概念はp5mopの仕組みに合う？」というを夜中すぎまで論じました。" };
-p { "[\@robinsmidsrod](http://twitter.com/robinsmidsrod)さんが「日本語を入力する方法を説明しませんか？」と聞いて、私はもちろん嬉しくなりました ;) その後、XPathとか[XML::Rabbit]とか[Unicode CLDR](http://ja.wikipedia.org/wiki/Common_Locale_Data_Repository)とかとか喋りました。" };
+p { "[\@robinsmidsrod](http://twitter.com/robinsmidsrod)さんが「日本語で入力する方法を説明しませんか？」と聞いて、私はもちろん嬉しくなりました ;) その後、XPathとか[XML::Rabbit]とか[Unicode CLDR](http://ja.wikipedia.org/wiki/Common_Locale_Data_Repository)とかとかについて喋りました。" };
 
 h3 { "Day 1 (登山の日)" };
 p { "早起きして、プレーケストーレンという有名な崖にハイキングしました。眺めはとっっってもきれいでした！！！" };
-p { "旅立ちは10時から16時までかかりました。たいへん疲れました。（三日後でもまだ足が痛い！）" };
+p { "旅立ちは10時から16時までかかりました。たいへん疲れました。（四日後でもまだ足が痛い！）" };
 
 a {
     href is "http://www.flickr.com/photos/sartak/7880432218";
@@ -77,7 +77,7 @@ a {
 };
 
 
-p { "その後で、Fluxx(zombie版とcthulhu版組み合わせられた)というカードゲームをやりました。計画者はプログラマーですから、法則は面白くて、進化することまであります。" };
+p { "その後で、[Fluxx](http://ja.wikipedia.org/wiki/フラックス_(ゲーム))(zombie版とcthulhu版組み合わせられた)というカードゲームをやりました。計画者はプログラマーですから、法則が面白い。しかもルールが進化することまであります。" };
 
 a {
     href is "http://www.flickr.com/photos/sartak/7880437426";
@@ -95,10 +95,10 @@ a {
 };
 
 h3 { "Day 2 (ハッカソンの一日目)" };
-p { "朝食から昼食まで私が「Mooseロール利用パターン」というトーク拡張版を話してあげました。昼食の後で、[\@pmichaud](http://twitter.com/pmichaud)さんが[NQP言語](http://pmichaud.com/2012/pres/mtmh2012-nqp/slides/start.html)について解説しました。プログラミング言語が作りたい方は、きっとNQPでとても早く計画できると思います。しかもハッカソンの間で[\@pmichaud](http://twitter.com/pmichaud)さんが「ちゃんとドキュメンテーションが書きたい」と言いました。" };
+p { "朝食から昼食まで私が「Mooseロール利用パターン」というトーク拡張版を話してあげました。昼食の後で、[\@pmichaud](http://twitter.com/pmichaud)さんが[NQP言語](http://pmichaud.com/2012/pres/mtmh2012-nqp/slides/start.html)について解説しました。プログラミング言語が作りたい方は、NQPを使うことで、きっととても早く計画できると思います。" };
 p { "[\@perlyarg](http://twitter.com/perlyarg)さんとMooseのロールテストをp5mopに変更して始まりました。p5mopではロール合成は不完全ですから、そのテストは便利だと思います。" };
-p { "最初のp5mopの拡張として[mopx::instance::tracking](https://github.com/stevan/p5-mop/commit/04997c0c93c7)は、私が書いたことができました。とても簡単に、p5mopのClassというメタクラスをサブクラスして、create_instanceというメソッドにオブジェクトを見逃さない挙動を追加されます。" };
-p { "夕飯の後で、昨日の旅だし、朝のトークをしたから、たいへん疲れました。で、やったことはFluxxだけでした。" };
+p { "最初のp5mopの拡張として[mopx::instance::tracking](https://github.com/stevan/p5-mop/commit/04997c0c93c7)は、私が書いたことができました。Moose版よりも簡単でした。p5mopのClassというメタクラスをサブクラスして、create_instanceというメソッドにオブジェクトを見逃さない挙動を追加されます。" };
+p { "夕飯の後で、昨日の旅だし、朝のトークをしたから、たいへん疲れました。で、私がやったことは、Fluxxだけでした。" };
 
 a {
     href is "http://www.flickr.com/photos/sartak/7880533504";
@@ -111,9 +111,9 @@ a {
 };
 
 h3 { "Day 3 (ハッカソンの二日目)" };
-p { "先日書いたDTraceプローブをPerlに追加する[パッチ](https://rt.perl.org/rt3/Ticket/Display.html?id=114638)をやっと送りました。もし受け入れたら、5.18からop-entry、loading-file、loaded-fileというプローブを使うようになります。" };
-p { "Rubenさんkが「正規表現を使いこなす教師いますかー！」と聞いて、「私が手伝ってみてあげましょう」と私は答えました。問題は、テストが9秒以上で実行するので、ちょっと遅すぎました。役800回呼び出したregcomp(正規表現をコンパイルする関数)というopcodeに2秒以上部分があることを[Devel::NYTProf]で見つけました。でも、ちゃんと`qr//`を使ったので、regcompがほとんど呼ばないはずだと思いました。残念ですがDevel::NYTProfがopcode内に見ることができませんが、DTraceは各C関数でも見られます。そして私がperl providerのsub-entryとsub-returnというプローブをトレースして、pid providerのC関数のentryとreturnで、遅いマッチだけ内regcompをトレースしました。発見として、700部分のregcompは速いですけど、あと100部分のregcompはその速いregcompより100x以上遅い。なに。。。？後程、遅すぎた正規表現は記録括弧がありまして、その括弧を`(?:...)`に変更することで、とてみ速くなったことができました！実行は9秒から2秒まで減りました。" };
-p { "AUTOLOADを使っているモジュールには、「URI::Namespace=HASH(0x7ff5fa032360) contains invalid characters for a type name. Names can contain alphanumeric character, \":\", and \".\"」というエラーが発生していました。結局、原因は`\$obj->type`呼び出して、AUTOLOADを実行するはずがあるけど、実は名前として`\$obj`を渡して、タイプを定義する`Moose::Util::TypeConstraint::type(\$obj)`を呼び出されました。`no Moose::Util::TypeConstraint`とか`use namespace::autoclean`を追加すると、typeというメソッドはAUTOLOADを実行するので、よく解決しました。" };
+p { "先日書いたDTraceプローブをPerlに追加する[パッチ](https://rt.perl.org/rt3/Ticket/Display.html?id=114638)をやっと送りました。p5pが受け入れたので、5.18からop-entry、loading-file、loaded-fileというプローブを使うようになります。" };
+p { "[\@RubenVerborgh](http://twitter.com/RubenVerborgh)さんkが「正規表現を使いこなす教師いますかー！」と聞いて、「私が手伝ってみてあげましょう」と私は答えました。問題は、テストが9秒以上で実行するので、ちょっと遅すぎました。役800回呼び出したregcomp(正規表現をコンパイルする関数)というopcodeに2秒以上部分があることを[Devel::NYTProf]で見つけました。でも、ちゃんと`qr//`を使ったので、regcompがほとんど呼ばないはずだと思いました。残念ですがDevel::NYTProfがopcode内に見ることができませんが、DTraceは各C関数でも見られます。そして私がperl providerのsub-entryとsub-returnというプローブをトレースして、pid providerのC関数のentryとreturnで、遅いマッチだけ内regcompをトレースしました。発見として、700部分のregcompは速いですけど、あと100部分のregcompはその速いregcompより100x以上遅い。なに。。。？後程、遅すぎた正規表現は記録括弧がありまして、その括弧を`(?:...)`に変更することで、とてみ速くなったことができました！実行は9秒から2秒まで減りました。" };
+p { "もう一つ問題に手伝ってあげました。AUTOLOADを使っているモジュールには、「URI::Namespace=HASH(0x7ff5fa032360) contains invalid characters for a type name. Names can contain alphanumeric character, \":\", and \".\"」というエラーが発生していました。結局、原因は`\$obj->type`呼び出して、AUTOLOADを実行するはずがあるけど、実は名前として`\$obj`を渡して、タイプを定義する`Moose::Util::TypeConstraint::type(\$obj)`を呼び出されました。`no Moose::Util::TypeConstraint`とか`use namespace::autoclean`を追加すると、typeというメソッドはAUTOLOADを実行するので、よく解決しました。" };
 
 a {
     href is "http://www.flickr.com/photos/sartak/7880605988";
@@ -127,8 +127,8 @@ a {
 
 h3 { "Day 4 (ハッカソンの三日目)" };
 
-p { "p5mopは`has \$foo`宣言でアトリビュート定義します。メソッドを実行する時に、PadWalkerでオブジェクトの値をその変数に代入します。メソッド内には、正しく変数を使うことができます。でも、\@doysterさんたちがbootstrapを書きなおした後で、\$self(PadWalkerで代入された変数)は時々間違うオブジェクトにあるが\$::SELF(localで代入された変数)は正しいオブジェクトにあることになってしまいました。\@doysterと\@stevanlittleと\@nothingmuchと私とデバッグしようとしましたが、結局原因を見つかれませんでした。" };
-p { "しかし、\@perlditionさんが書いている新しいモジュールで解決できるはずです。そのモジュールはlexical padで手動的に管理できますが、メソッド実行時に、カスタムpadをプッシュし、オブジェクトの値を使い、カスタムpadをパップすることで、PadWalker壊すことを抜けるはずです。" };
+p { "p5mopは`has \$foo`宣言でアトリビュート定義します。メソッドを実行する時に、PadWalkerでオブジェクトの値をその変数に代入します。メソッド内には、正しく変数を使うことができます。でも、[\@doyster](http://twitter.com/doyster)さんたちがbootstrapを書きなおした後で、\$self(PadWalkerで代入された変数)は時々間違うオブジェクトにあるが\$::SELF(localで代入された変数)は正しいオブジェクトにあることになってしまいました。\@doysterと[\@stevanlittle](http://twitter.com/stevanlittle)と[\@nothingmuch](http://twitter.com/nothingmuch)と私とデバッグしようとしましたが、結局原因を見つかれませんでした。" };
+p { "しかし、[\@perldition](http://twitter.com/perldition)さんが書いている新しいモジュールにすることで、解決できるはずです。そのモジュールはlexical padで手動的に管理できます。具体的に、メソッド実行時に、カスタムpadをスタックにプッシュし、そのブロックがオブジェクトの値を使えるになります。メソッドが返す時に、カスタムpadをポップされます。このモジュールでは、変数汚染が抜けられるはずです。" };
 
 a {
     href is "http://www.flickr.com/photos/sartak/7886214156";
@@ -136,3 +136,7 @@ a {
 };
 
 h3 { "Day 5 (帰宅の日)" };
+p { "空港まで無事に着きました。でも、チェックインしようとしたら、「目的地：London/Gatwick」と表示されました。クソォォ！！と思って、不安感じきました。London/*Heathrow*からボストンまで飛ぶ飛行に乗り換える予定たったのです。Heathrowのターミナル5に乗り換えるのは、二時間でもちょっと足りないのですね。GatwickからHeathrowまで乗り換えるのは絶対無理です！原因は、予約には「London」だけ示して、ロンドンは航空5つまであると、全然考えなかったのです。" };
+p { "とにかく、カスタマーサービスに「飛行機の予約を変更してくれないか？」と聞いて、「新しい予約を取るしかできません」と答えました。で、新しくて高い予約を取りました。フランクフルトの代わりに、オスロで乗り換えまして、目的地はHeathrowにしました。"　};
+p { "Heathrowでビールと二番好きなチキンカツカレーを食べました。結局、定刻通りに家に帰ったことができました。" };
+
