4f33057a1f86e1ba194520ad167fa1ebfaf26eaf
Shawn M Moore <code@sartak.org>
2013-02-07 16:58:37 -0500

    git-flux images

---
+++ writing/git-flux.pl
@@ -0,0 +1,8 @@
+use Sartak::Blog;
+
+BEGIN { print "title: git flux\ndraft: 1\n" }
+
+img { src is "/tmp/ideal-release.png" };
+img { src is "/tmp/changes-to-base.png" };
+img { src is "/tmp/midcycle-hotfix.png" };
+
