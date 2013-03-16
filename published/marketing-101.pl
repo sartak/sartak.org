263d6dfe9b29be39540a4cc0932bab20fed040eb
Shawn M Moore <code@sartak.org>
2013-03-16 13:55:30 -0400

    Marketing 101

--- published/marketing-101.pl
+++ published/marketing-101.pl
@@ -1,11 +1,11 @@
 use Sartak::Blog;
 
-BEGIN { print "title: Marketing 101\ndraft: 1\n" }
+BEGIN { print "title: Marketing 101\ndate: 2013-03-16\n" }
 
-p { "One of my girlfriend's friends just messaged her:" };
+p { "My girlfriend's friend just sent her a message:" };
 
 blockquote {
-    p { q{I'm reading an interesting article about the founders of GitHub. I was wondering "Where have I heard about this?"} };
+    p { q{I'm reading an interesting article about the founders of GitHub. I was wondering, "Where have I heard about this?"} };
     p { "It's imprinted on my brain from all those hungover mornings drinking coffee out of that mug!" };
 };
 
