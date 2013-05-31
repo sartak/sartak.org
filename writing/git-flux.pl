40a30e0407c63db927acfe49575ae2afbf35c0e8
Shawn M Moore <code@sartak.org>
2013-05-31 10:05:32 -0400

    Clear out my unpublished crud

--- writing/git-flux.pl
+++
@@ -1,8 +0,0 @@
-use Sartak::Blog;
-
-BEGIN { print "title: git flux\ndraft: 1\n" }
-
-img { src is "/tmp/ideal-release.png" };
-img { src is "/tmp/changes-to-base.png" };
-img { src is "/tmp/midcycle-hotfix.png" };
-
