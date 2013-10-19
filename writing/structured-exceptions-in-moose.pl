75778e848289fb40fb8663bdec2e1b9806ea363c
Shawn M Moore <code@sartak.org>
2013-10-19 15:36:35 -0400

    WIP

---
+++ writing/structured-exceptions-in-moose.pl
@@ -0,0 +1,16 @@
+p { "Yesterday, Upasana Shukla's structured exceptions branch [landed in Moose](https://github.com/moose/Moose/pull/38). Her apprenticeship is now officially complete. I couldn't be happier with her work." };
+
+h2 { "Backstory" };
+
+p { "At YAPC::NA 2012 in Madison, [John Anderson](https://twitter.com/genehack) gave a [talk on the benefits of structured exceptions](http://www.youtube.com/watch?v=O1b-z7QvVW0). We all lamented Moose's stringy exceptions several times. It was especially egregious because the rest of Moose is so heavily object-oriented, having been built on top of a meta-object protocol. During the QA for John's talk it was made clear that if anyone was willing to put in the hard work to convert Moose to structured exceptions, that branch would not only be merged, but cherished." };
+
+p { "During the conference I sort of volunteered for that work. I started a branch and never really went anywhere with it. That led to ribbing over the next six months in the Moose IRC channels that I hadn't been working on the branch." };
+
+p { "Then in March 2013, Karen Pauley asked me out of the blue:" };
+
+blockquote { "I noticed that you were listed as a mentor for previous GSoC projects and I wondered if you would mind being a possible mentor for this program?" };
+
+p { "I said yes, since I of course had a very specific project in mind, one that would take about a summer's worth of work. " };
+
+
+p { "As far as I know, Upasana plans to come to YAPC::NA 2014 in Orlando. It would be a real shame if she had to pay for *any* of her food. :)" };
