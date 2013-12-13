4a8e7c2b6f8a8d5ca6982406c365c820fd07f46b
Shawn M Moore <code@sartak.org>
2013-12-13 14:06:07 -0500

    IIDelegate: Conforming to Protocols with Blocks

---
+++ published/conforming-to-protocols-with-blocks.pl
@@ -0,0 +1,4 @@
+use Sartak::Blog;
+
+BEGIN { print "title: IIDelegate: Conforming to Protocols with Blocks\ndate: 2013-12-12\nexternal: http://iinteractive.com/notebook/2013/12/12/iidelegate.html\n" }
+
