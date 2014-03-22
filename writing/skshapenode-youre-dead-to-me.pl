b48ce9c434077adae463da20223ca7f3c98e85b8
Shawn M Moore <shawn.moore@iinteractive.com>
2014-03-22 03:19:11 -0400

    WIP

--- writing/skshapenode-youre-dead-to-me.pl
+++ writing/skshapenode-youre-dead-to-me.pl
@@ -4,9 +4,9 @@ BEGIN { print "title: <tt>SKShapeNode</tt>, you're dead to me
 draft: 1
 " }
 
-p { "For the past three months I've spent damn near every night and weekend moment building my next iOS game. I now regularly shut down [Diesel](http://www.diesel-cafe.com) at 11pm. The game is my most ambitious project yet and I'm having a blast making it. I've not got anything to announce just yet. Soon! In the meantime, I want to describe one recurring source of pain in [Sprite Kit](https://developer.apple.com/library/ios/documentation/GraphicsAnimation/Conceptual/SpriteKit_PG/Introduction/Introduction.html)." };
+p { "For the past three months I've spent damn near every night and weekend moment building my next iOS game. I now regularly shut down [Diesel Cafe](http://www.diesel-cafe.com). The game is my most ambitious project yet and I'm having a blast making it. I'm not ready to announce it yet. For now I just want to describe one recurring source of pain in [Sprite Kit](https://developer.apple.com/library/ios/documentation/GraphicsAnimation/Conceptual/SpriteKit_PG/Introduction/Introduction.html)." };
 
-p{ "**`SKShapeNode`** is a subclass of `SKNode` that lets you draw arbitrary shapes. Bézier curves, polygons, rings, whatever. You can set the stroke color of the shape, or its fill color, or both. This is certainly useful behavior. You could probably implement a decent chunk of your game's HUD with it. Bézier curves are a great way to give visual feedback of a user's gesture as in, say, [Flight Control](https://itunes.apple.com/us/app/flight-control/id306220440?at=11l7vX&ct=sartak-shape). Describing shapes at runtime rather than at design time (as in `SKSpriteNode`) unlocks worlds of possibilities." };
+p{ "**`SKShapeNode`** is a subclass of `SKNode` that draws a `CGPathRef`. It can render Bézier curves, polygons, rings, Louisiana, whatever. You can set the stroke color of the shape, or its fill color, or both. You could probably implement a decent chunk of your game's HUD with it. Bézier curves are a great way to give visual feedback of a user's gesture as in, say, [Flight Control](https://itunes.apple.com/us/app/flight-control/id306220440?at=11l7vX&ct=sartak-shape). Describing shapes at runtime rather than at design time (as in `SKSpriteNode`) unlocks worlds of possibilities." };
 
 p { "However, `SKShapeNode` is by far the least-well engineered API in Sprite Kit. In fact, I have trouble naming a single lousier API that I've used since I started programming professionally. At least SOAP has an _ethos_." };
 
@@ -23,12 +23,12 @@ ol {
     li {
         id is "linewidth";
         p { qq{From `SKShapeNode`'s [documentation](https://developer.apple.com/library/ios/documentation/SpriteKit/Reference/SKShapeNode_Ref/Reference/Reference.html#//apple_ref/occ/instp/SKShapeNode/lineWidth), "A line width larger than `2.0` may cause rendering artifacts in the final rendered image."} };
-        p { "Wat." };
+        p { "It's good that they are up front about this limitation. But that is a heavy restriction." };
     }
 
     li {
         id is "setstrokecolor";
-        p { "When I send a message like `setStrokeColor:[SKColor redColor]`, it sometimes has no visual effect at all. So I have to trick the `SKShapeNode` into redrawing itself. Changing its `alpha` is one way to do it:" };
+        p { "Sometimes `setStrokeColor:[SKColor redColor]` has no visual effect at all. So you have to trick the `SKShapeNode` into redrawing itself. Changing its `alpha` is one way to do it:" };
 
 code_snippet 'objc' => << 'CODE';
 #if BUSTED_SKSHAPENODE_SETSTROKECOLOR
@@ -36,7 +36,7 @@ code_snippet 'objc' => << 'CODE';
     shape.alpha = 0;
     shape.alpha = oldAlpha;
 #endif
-shape.strokeColor = [SKColor redColor];
+    shape.strokeColor = [SKColor redColor];
 CODE
 
         p { "Note that it is _not_ sufficient to simply say `shape.alpha = shape.alpha`. That does not trigger a display. For whatever reason, the internals demand you actually change the property value." };
@@ -45,7 +45,7 @@ CODE
 
         br {};
 
-        p { "Digging deeper, it seems this problem manifests itself only when the `SKShapeNode` is a descendent of an `SKEffectNode`. To see it in action, start a new project using the Sprite Kit template and replace your scene class's implementation with this:" };
+        p { "Digging deeper, it seems this problem manifests itself only when the `SKShapeNode` is a descendent of `SKEffectNode`. To see it in action, start a new project using the Sprite Kit template and replace your scene class's implementation with this:" };
 
 code_snippet 'objc' => << 'CODE';
 -(id)initWithSize:(CGSize)size {
@@ -74,7 +74,7 @@ code_snippet 'objc' => << 'CODE';
 }
 CODE
 
-        p { "For me, the round rect stays red indefinitely. If you change that `if (0)` to a true value, then the code to change the `SKShapeNode`'s alpha kicks in and that causes the subsequent `setStrokeColor:` to have the intended visible effect." };
+        p { "For me, the round rect stays red indefinitely. If you change that `if (0)` to a true value, then the alpha change causes the subsequent `setStrokeColor:` to have the intended visible effect." };
     }
 
     li {
@@ -87,7 +87,7 @@ CODE
             src is "/img/blog/skshapenode-youre-dead-to-me/detritus.png";
         };
 
-        p { "Those red lines are from `SKShapeNode` instances that once rendered red-stroked round rects. _Many_ frames ago. For whatever reason `SKShapeNode` decided to try to resurrect them, but only did half the job." };
+        p { "Those red lines are from `SKShapeNode` instances that once rendered red rectangles. _Many_ frames ago. For whatever reason `SKShapeNode` decided to try to resurrect them, but only did half the job." };
     };
     li {
         id is "resizing";
@@ -104,7 +104,8 @@ CODE
             height is "568";
             src is "/img/blog/skshapenode-youre-dead-to-me/shrink-post.png";
         };
-        p { "I have no idea why this happens except it seems to yet again be the fault of `SKShapeNode` inside of an `SKEffectNode`. Luckily I've been able to replicate it with a pretty small amount of code. As before, replace the Sprite Kit template's scene class's implementation with the following:" };
+        p { "I have no idea why this happens except it seems to yet again be the fault of `SKShapeNode` inside of an `SKEffectNode`. The cause is possibly related to `SKEffectNode`'s unique rendering model. `SKEffectNode` lets you apply Core Image filters (which are akin to Photoshop filters) to some of your nodes. It's amazingly powerful. Seriously next level shit. But to achieve that, it must render its subtree into a separate buffer to which it can apply CI filters. This different codepath is probably the cause of all the problems. But if `SKShapeNode` cannot handle being rendered in an `SKEffectNode`, I seriously question how robust Sprite Kit is. (Incidentally, `SKEffectNode` also doesn't respect the `zPosition`s of its children, but that's another post altogether. The solution for that one is to interject a plain `SKNode` into the node tree.)" };
+        p { "Anyway. I've luckily been able to replicate this crazy rendering bug with a small amount of code. As before, replace the Sprite Kit template's scene class's implementation with the following:" };
         code_snippet 'objc' => << 'CODE';
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
@@ -137,10 +138,13 @@ p { "Because of all these flaws, **`SKShapeNode` is completely untrustworthy**. 
 
 ol {
     li {
+        id is "nuke";
+        p { "Just remove the `SKShapeNode`. For some effects it's not worth all the trouble." };
+    };
+    li {
         id is "colorsprite";
-        p { "For borders on opaque nodes, just use a `SKShapeNode` instantiated with `+[SKSpriteNode spriteNodeWithColor:size:]`. This gets you a rectangular block of the provided `SKColor`." };
-        p { "Your borders will look better too. And you won't have to fear using a border width of greater than 2.0." };
-        p { "*Cripes*." };
+        p { "For borders on opaque nodes, just use a `SKShapeNode` instantiated with `+[SKSpriteNode spriteNodeWithColor:size:]`. This gets you a rectangular block of the provided `SKColor`. Beyond just borders, I've converted my HP bars this way too." };
+        p { "The output even looks better too. And you won't have to fear using a border width of greater than 2.0. Cripes!" };
     };
 
     li {
