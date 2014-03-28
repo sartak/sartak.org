8990be0477d3e85bb41b734822fa57ae0a6e37bd
Shawn M Moore <shawn.moore@iinteractive.com>
2014-03-28 13:50:03 -0400

    conducty

---
+++ published/conducty-a-12-conduct-nethack-ascension.pl
@@ -0,0 +1,4 @@
+use Sartak::Blog;
+
+BEGIN { print "title: Conducty: a 12-conduct NetHack Ascension\ndate: 2007-04-14\nexternal: https://groups.google.com/d/msg/rec.games.roguelike.nethack/0h3NBVQk-fY/awo4PcJXq5MJ\n" }
+
