c31ea76b0f4af8ce9b48b804149f08fbeee230b0
Shawn M Moore <shawn.moore@iinteractive.com>
2014-04-10 18:34:13 -0400

    Done?

--- writing/compile-time-error-for-incorrectly-cased-import.pl
+++ writing/compile-time-error-for-incorrectly-cased-import.pl
@@ -12,19 +12,19 @@ CODE
 
 p { "Gawsh, I can't put my finger on it, but that kinda rubs me the wrong way." };
 
-p { "One nice thing about working in a language like Objective-C (relative to my mother tongue Perl, anyway) is that I am immediately notified of most typos. The frequency of my typing `YWaction.h` when I meant `YWAction.h` would frighten children. The real tragedy of this is that clang does not warn about this. It's just just plain sloppy, which I cannot abide. Practically, it would also lead to lots of tiny problems should anyone try to build this app on a case-sensitive filesystem." };
+p { "One of my favorite things about working in a language like Objective-C (relative to my mother tongue Perl, anyway) is that I am immediately notified of most typos. But clang does not warn about this one. The frequency of my typing `YWaction.h` when I meant `YWAction.h` would frighten children. It's just plain sloppy. That I cannot abide. Practically, it would also lead to lots of tiny problems should anyone try to build this app on a case-sensitive filesystem." };
 
-p { "So, let's put this high-falutin' typin' teevee machine to work. Here is how you can get [Xcode to text you](http://www.textfromxcode.com) more when you screw up your imports." };
+p { "Carefully reviewing all the `#import` statements in my project makes my eyes glaze over. So, let's put this high-falutin' typin' teevee machine to work. Here is how you can get [Xcode to text you](http://www.textfromxcode.com) more when you screw up your imports." };
 
-ul {
+ol {
     li { "Navigate to your app target. It's probably the first entry in your file navigator, then under Targets on the left pane." };
     li { "Select the Build Phases tab." };
     li { "Click the little `+` button at the top left." };
     li { 'Select "New Run Script Build Phase".' };
     li { 'This adds a "Run Script" entry to the bottom of this list. Pop it open by clicking its disclosure triangle.' };
     li { "Set the value of Shell to `/usr/bin/perl`" }
-    li { "You heard that right. Perl." };
-    li { outs "In the textarea below Shell, paste in the following Perl script: ";
+    li { "You heard that right. _Perl_." };
+    li { outs "In the text field below Shell, paste in the following Perl script: ";
 
 code_snippet 'perl' => << 'CODE';
 my @files = glob("*/*.[hm]");
@@ -54,14 +54,25 @@ for my $file (@files) {
 exit 1 if $errors;
 CODE
    };
-   li { "Rename the build phase by clicking its name twice. I called mine `Check #import Casing`" };
-   li { "Drag and drop your build phase wherever you like. Mine's near the top, because I think it's better to fail fast." };
+   li { "Rename the build phase by clicking its name twice. I called mine `Check #import Casing`." };
+   li { "Drag and drop to reorder your build phases however you like. Mine's near the top, because I think it's better to fail fast." };
 };
 
 p { "When it's all said and done, your build phase should resemble mine. Unless you're from the future, in which case [you look different!](https://www.youtube.com/watch?v=3SbeIqPhtSk)" };
 
 img {
-    width is "625";
-    height is "699";
+    width is "627";
+    height is "696";
     src is "/img/blog/compile-time-error-for-incorrectly-cased-import/build-phase.png";
 };
+
+p { "Now when you `⌘B`, Xcode will tell you about all your miscased `#import` statements just like any other builtin error. It even shows these errors right in context." };
+
+img {
+    width is "858";
+    height is "128";
+    src is "/img/blog/compile-time-error-for-incorrectly-cased-import/sweet-sweet-error.png";
+};
+
+p { "For me, this is an error because all warnings are errors in my project. MTFU." };
+
