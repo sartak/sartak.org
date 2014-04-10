69942aa507a3e05f31478636ff1d89d0bc886403
Shawn M Moore <shawn.moore@iinteractive.com>
2014-04-10 18:40:38 -0400

    Done!

--- writing/compile-time-error-for-incorrectly-cased-import.pl
+++ writing/compile-time-error-for-incorrectly-cased-import.pl
@@ -12,7 +12,7 @@ CODE
 
 p { "Gawsh, I can't put my finger on it, but that kinda rubs me the wrong way." };
 
-p { "One of my favorite things about working in a language like Objective-C (relative to my mother tongue Perl, anyway) is that I am immediately notified of most typos. But clang does not warn about this one. The frequency of my typing `YWaction.h` when I meant `YWAction.h` would frighten children. It's just plain sloppy. That I cannot abide. Practically, it would also lead to lots of tiny problems should anyone try to build this app on a case-sensitive filesystem." };
+p { "The frequency of my typing `YWaction.h` when I meant `YWAction.h` would frighten children. One of my favorite things about working in a language like Objective-C (relative to my mother tongue Perl, anyway) is that I am immediately notified of most typos. But clang does not warn about this one. It's just plain sloppy. That I cannot abide. Practically, it would also lead to lots of tiny problems should anyone try to build this app on a case-sensitive filesystem." };
 
 p { "Carefully reviewing all the `#import` statements in my project makes my eyes glaze over. So, let's put this high-falutin' typin' teevee machine to work. Here is how you can get [Xcode to text you](http://www.textfromxcode.com) more when you screw up your imports." };
 
@@ -66,7 +66,7 @@ img {
     src is "/img/blog/compile-time-error-for-incorrectly-cased-import/build-phase.png";
 };
 
-p { "Now when you `⌘B`, Xcode will tell you about all your miscased `#import` statements just like any other builtin error. It even shows these errors right in context." };
+p { "Now when you `⌘B`, Xcode will tell you about all your miscased `#import` statements just like any other builtin error. One less thing to be vigilant about! Happily, Xcode even shows these errors right in context." };
 
 img {
     width is "858";
@@ -74,5 +74,5 @@ img {
     src is "/img/blog/compile-time-error-for-incorrectly-cased-import/sweet-sweet-error.png";
 };
 
-p { "For me, this is an error because all warnings are errors in my project. MTFU." };
+p { "You might notice that this is actually an error. That's because in my projects, all warnings are errors. Ain't nobody got time for anything less." };
 
