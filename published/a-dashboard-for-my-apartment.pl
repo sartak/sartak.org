ad041c48439056e47e28254a16d7be7216f4b213
Shawn M Moore <shawn.moore@iinteractive.com>
2015-03-06 12:04:50 -0500

    Add my new post

---
+++ published/a-dashboard-for-my-apartment.pl
@@ -0,0 +1,5 @@
+use Sartak::Blog;
+
+BEGIN { print "title: A Dashboard for My Apartment\ndate: 2015-03-06\nexternal: http://iinteractive.com/notebook/2015/03/06/a-dashboard-for-my-apartment.html\n" }
+
+
