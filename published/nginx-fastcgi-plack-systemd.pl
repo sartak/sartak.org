eb447005eed39a7dc42fec78b5f79376a7778e51
Shawn M Moore <shawn@bestpractical.com>
2015-12-02 01:48:45 -0500

    Better words

--- published/nginx-fastcgi-plack-systemd.pl
+++ published/nginx-fastcgi-plack-systemd.pl
@@ -4,7 +4,7 @@ BEGIN { print "title: nginx + FastCGI + Plack + systemd
 date: 2015-12-02
 " }
 
-p { "One of my backburner projects has been to migrate this here website to a fresh server. I never want to feel stuck on an old server again, so I've been investing time to make deploying the full sartak.org experience both quick and painless. That means managing all the configuration with the configuration manager I hate the least. Today that happens to be Ansible." };
+p { "One of my backburner projects has been to migrate this here website onto a fresh server. I never want to feel stuck on an old server again, so I've been investing time to make deploying the full sartak.org experience both quick and painless. That means managing all the configuration with the configuration manager I hate the least. Today that happens to be Ansible." };
 
 p { "As part of this betterment, I have decided that the following is no longer an acceptable way to manage web applications:"};
 
@@ -20,7 +20,7 @@ p { "I do admit this setup hasn't caused me much grief (probably thanks to how r
 
 p { "Since I'm deploying to the latest Debian, I've decided to give systemd a try. It was a little tricky to find the right incantations but I've smuggled them out just for you." };
 
-p { "Here's what I'm working with here. I've got a small, light-traffic Perl web app called [`Sartak-Microblog`](https://github.com/sartak/Sartak-Microblog). Its code, git repository, and SQLite database all live in `/usr/local/share/Sartak-Microblog`, so that needs to be the current directory from which the app is launched. nginx will serve it up as [`http://micro.sartak.org`](http://micro.sartak.org), talking to Plack's FastCGI handler over a UNIX socket in `/tmp`. This doesn't need to be a Google-scale production. I don't need zero-downtime updates or hot spares or multi-datacenter redundancy. I'm also fine just letting Plack serve my static files itself." };
+p { "Here's what I'm working with here. I've got a small, light-traffic Perl web app called [`Sartak-Microblog`](https://github.com/sartak/Sartak-Microblog). Its code, git repository, and SQLite database all live in `/usr/local/share/Sartak-Microblog`, so that needs to be the current directory from which the app is launched. nginx will serve it up as [`http://micro.sartak.org`](http://micro.sartak.org), talking to Plack's FastCGI handler over a UNIX socket in `/tmp`. This doesn't need to be a Google-scale production. I don't need zero-downtime updates or hot spares or multi-datacenter redundancy. I just want the app to at least _try_ to stay running. Also, letting Plack itself serve my static files is plenty fine by me." };
 
 p { "First, the nginx config. (For those following along at home, replace both instances of `micro.sartak.org` with your domain name)" };
 
@@ -35,7 +35,7 @@ outs_raw << 'NGINX';
 }</pre>
 NGINX
 
-p { "Next is to convince systemd to keep the app running. Put this into `/etc/systemd/system/micro.sartak.org.service` (Replace `micro.sartak.org` and `/usr/local/share/Sartak-Microblog`, and possibly also `app.psgi`)" };
+p { "Next is to convince systemd to keep the app running. Drop this into `/etc/systemd/system/micro.sartak.org.service` (Replace `micro.sartak.org` and `/usr/local/share/Sartak-Microblog`, and possibly also `app.psgi`)" };
 
 code_snippet 'dosini' => << 'INI';
 [Unit]
