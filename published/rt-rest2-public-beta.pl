bf5d9f613de887b542282333b3b961e4d030ed12
Shawn M Moore <code@sartak.org>
2017-12-29 20:18:11 -0500

    Add REST2 site

---
+++ published/rt-rest2-public-beta.pl
@@ -0,0 +1,3 @@
+use Sartak::Blog;
+
+BEGIN { print "title: Request Tracker REST2 Public Beta\ndate: 2017-07-20\nexternal: https://bestpractical.com/blog/2017/7/rest2-public-beta-and-request-for-comment\n" }
